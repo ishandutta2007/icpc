5ca9a0fca6924b616f884d87458fdb6051369f09
Nero <286830682@qq.com>
2015-01-16 19:31:59 +0800

    counting

--- TopCoder/ColourHolic/ColourHolic.java
+++ TopCoder/ColourHolic/ColourHolic.java
@@ -3,95 +3,94 @@ import java.util.*;
 
 public class ColourHolic {
 	public int countSequences(int[] A) {
-		prepare();
 		Arrays.sort(A);
 		int a = A[0],b = A[1],c = A[2],d = A[3];
-		long answer = 0;
 		if (a == 0 && b == 0) {
 			if (c == d) return 2;
-			if (c+1 == d || c == d+1) return 1;
+			if (c+1 == d) return 1;
 			return 0;
-		} else if (a == 0) {
-			for (int i = 0; i <= b-1; ++ i) {
-				for (int j = 0; j <= c-1 && i+j <= d; ++ j) {
-					long t = 0;
-					if (b-i == c-j) {
-						t = 2;
-					} else if (b-i+1 == c-j || b-i == c-j+1) {
-						t = 1;
-					}
-					t = t * comb(b-1,i) % MOD * comb(c-1,j) % MOD;
-					int x = b-i+c-j;
-					int y = d-i-j;
-					if (x < y-1) continue;
-					t = t * comb(x-y+1+y,y) % MOD;
-					answer += t;
-					if (answer >= MOD) answer -= MOD;
-				}
-			}
-			return (int)answer;
-		} else {
-			long[] small = new long[a+b-1];
-			for (int i = 0; i <= a-1; ++ i) {
-				for (int j = 0; j <= b-1; ++ j) {
-					long t = 0;
-					if (a-i == b-j) {
-						t = 2;
-					} else if (a-i+1 == b-j || a-i == b-j+1) {
-						t = 1;
-					}
-					t = t * comb(a-1,i) % MOD * comb(b-1,j) % MOD;
-					small[i+j] += t;
-					if (small[i+j] >= MOD) small[i+j] -= MOD;
-				}
-			}
-			long[] big = new long[c+d-1];
-			for (int i = 0; i <= c-1; ++ i) {
-				for (int j = c-i-1; j <= c-i+1; ++ j) {
-					if (j <= 0) continue;
-					if (j > d) continue;
-					int k = d-j;
-					long t = 0;
-					if (c-i == j) {
-						t = 2;
-					} else {
-						t = 1;
-					}
-					t = t * comb(c-1,i) % MOD * comb(d-1,k) % MOD;
-					big[i+k] += t;
-					if (big[i+k] >= MOD) big[i+k] -= MOD;
-				}
-			}
-			return (int)answer;
 		}
+		{
+			int n = a + b + c + d + 5;
+			F = new long[n];
+			Finv = new long[n];
+			Inv = new long[n];
+			two = new long[n];
+			Inv[1] = 1;
+			for (int i = 2; i < n; ++ i) {
+				Inv[i] = (MOD-MOD/i) * Inv[MOD%i] % MOD;
+			}
+			F[0] = Finv[0] = 1;
+			for (int i = 1; i < n; ++ i) {
+				F[i] = F[i-1] * i % MOD;
+				Finv[i] = Finv[i-1] * Inv[i] % MOD;
+			}
+			two[0] = 1;
+			for (int i = 1; i < n; ++ i) {
+				two[i] = two[i-1] * 2 % MOD;
+			}
+		}
+		if (a == 0) {
+			return (int) calc(b,c,d);
+		}
+		long answer = 0;
+		for (int g = 1; g <= a+b; ++ g) {
+			long ways = 0;
+			for (int e = 0; e <= g; ++ e) {
+				if (e > a || e > b) break;
+				int tmp = g-e-a+b;
+				if (tmp < 0 || tmp%2 == 1) continue;
+				int ob = tmp / 2;
+				int oa = a-b+ob;
+				if (oa < 0 || oa > a || ob < 0 || ob > b) continue;
+				int r = a-e-oa;
+				if (r < 0) continue;
+				long value = comb(g,e) * comb(g-e,oa) % MOD
+					* two[e] % MOD * comb(g-1+r,r) % MOD;
+				ways += value;
+				ways %= MOD;
+			}
+			answer += ways * calc(g,c,d) % MOD;
+			answer %= MOD;
+		}
+		return (int) answer;
+	}
+
+	long calc(int a,int b,int c) {
+		long ret = 0;
+		for (int g = c-1; g <= c+1; ++ g) {
+			for (int e = 0; e <= Math.min(a,b); ++ e) {
+				if (e > g) break;
+				int tmp = g-e-a+b;
+				if (tmp < 0 || tmp%2 == 1) continue;
+				int ob = tmp / 2;
+				int oa = a-b+ob;
+				if (oa < 0 || oa > a || ob < 0 || ob > b) continue;
+				int r = a-e-oa;
+				if (r < 0) continue;
+				long value = comb(g,e) * comb(g-e,oa) % MOD
+					* two[e] % MOD * comb(g-1+r,r) % MOD;
+				if (g == c) {
+					value = value * 2 % MOD;
+				}
+				ret += value;
+				ret %= MOD;
+			}
+		}
+		return ret;
 	}
 
 	long comb(int a,int b) {
 		if (a < b) return 0;
-		return F[a] * Finv[b] % MOD * Finv[a-b] % MOD;
+		return F[a] * Finv[a-b] % MOD * Finv[b] % MOD;
 	}
 
-	void prepare() {
-		int n = 200000 + 5;
-		F = new long[n];
-		Finv = new long[n];
-		Inv = new long[n];
-		Finv[0] = F[0] = 1;
-		Inv[1] = 1;
-		for (int i = 2; i < n; ++ i) {
-			Inv[i] = (MOD-MOD/i) * Inv[MOD%i] % MOD;
-		}
-		for (int i = 1; i < n; ++ i) {
-			F[i] = F[i-1] * i % MOD;
-			Finv[i] = Finv[i-1] * Inv[i] % MOD;
-		}
-	}
+	long[] F,Finv,Inv,two;
+	static final int MOD = (int)1e9+9;
 
-	long[] F,Finv,Inv;
-	final int MOD = (int)1e9+9;
 
 	void debug(Object... o) {
-		System.err.println(Arrays.deepToString(o));
+		System.out.println(Arrays.deepToString(o));
 	}
 
 	// CUT begin
@@ -127,7 +126,7 @@ public class ColourHolic {
 		if (caseSet.size() > 0) cases = caseSet.size();
 		System.err.println(String.format("%nPassed : %d/%d cases", passed, cases));
 
-		int T = (int)(System.currentTimeMillis() / 1000) - 1417349601;
+		int T = (int)(System.currentTimeMillis() / 1000) - 1420780442;
 		double PT = T / 60.0, TT = 75.0;
 		System.err.println(String.format("Time   : %d minutes %d secs%nScore  : %.2f points", T / 60, T % 60, 1000 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))));
 	}
