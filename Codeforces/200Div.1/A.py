c7434b3f369f2dbf14d50632f502968335debd66
Nero <286830682@qq.com>
2015-03-16 11:38:13 +0800

    delete cf and hdu

--- Codeforces/200Div.1/A.py
+++
@@ -1,5 +0,0 @@
-def f(a,b):
-        if b==0: return 0
-        else: return a/b + f(b,a%b)
-a,b = map(int,raw_input().split())
-print f(a,b)
--- Codeforces/200Div.1/B.py
+++
@@ -1,5 +0,0 @@
-s,a = raw_input(),[]
-for c in s:
-        if len(a) and a[-1]==c: del a[-1]
-        else: a.append(c)
-print ('NO','YES')[len(a)==0]
