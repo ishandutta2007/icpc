2bfc4e239a9eb79a36ccc706c19dd08a464e18c9
Nero <286830682@qq.com>
2015-04-07 20:09:52 +0800

    fast read

---
+++ Template/fast_read.cpp
@@ -0,0 +1,7 @@
+template<typename T>
+inline void getInt(T &x) {
+    char c; bool sign = false;
+    for (c = getchar(); c < '0' || c > '9'; c = getchar()) if (c == '-') sign = true;
+    for (x = 0; c >= '0' && c <= '9'; c = getchar()) x = x * 10 + c - '0';
+    sign && (x=-x);
+}
