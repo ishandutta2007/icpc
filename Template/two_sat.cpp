8c6e94c82731fa74ed8f87d306a05b747d831c37
Nero <286830682@qq.com>
2015-04-07 18:42:42 +0800

    two sat (lrj)

---
+++ Template/two_sat.cpp
@@ -0,0 +1,36 @@
+template<int N> 
+struct Sat {
+    int head[N],etot,stack[N],top;
+    bool mark[N];
+    struct Edge {int v,next;} g[501000];
+    void init() {
+        memset(head,-1,sizeof(head)); etot = 0;
+    }
+    void addEdge(int u,int v) {
+        g[etot].v = v; g[etot].next = head[u]; head[u] = etot ++;
+    }
+    bool dfs(int u) {
+        if (mark[u^1]) return false;
+        if (mark[u]) return true;
+        mark[u] = true;
+        stack[top++] = u;
+        for (int i = head[u]; i != -1; i = g[i].next) {
+            int v = g[i].v;
+            if (!dfs(v)) return false;
+        }
+        return true;
+    }
+    bool work(int n) {
+        memset(mark,0,sizeof(mark));
+        for (int i = 0; i < n; i += 2) {
+            if (!mark[i] && !mark[i+1]) {
+                top = 0;
+                if (!dfs(i)) {
+                    while (top) mark[stack[--top]] = false;
+                    if (!dfs(i+1)) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
