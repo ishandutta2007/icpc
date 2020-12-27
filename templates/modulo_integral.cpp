45fe7fd946c19654ae5e0104d36739c5677baa8a
nero <286830682@qq.com>
2020-12-28 02:52:26 +0800

    Add binomial<MOD>

--- templates/modulo_integral.cpp
+++ templates/modulo_integral.cpp
@@ -1,3 +1,11 @@
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include "debuger.hpp"
+#else
+#define DUMP(...) 1145141919810
+#define CHECK(...) (__VA_ARGS__)
+#endif
+
 template<int MOD>
 struct Integral {
   int v_ = 0;
@@ -31,3 +39,42 @@ struct Integral {
   }
   Integral inv() const { return power(MOD - 2); }
 };
+
+template<int MOD>
+struct Binomial {
+  std::vector<Integral<MOD>> factor, inv_factor;
+
+  void init(int n) {
+    factor.resize(n + 1);
+    inv_factor.resize(n + 1);
+    factor[0] = 1;
+    for (int i = 1; i <= n; ++i) factor[i] = factor[i - 1] * i;
+    inv_factor[n] = factor[n].inv();
+    for (int i = n; i >= 1; --i) inv_factor[i - 1] = inv_factor[i] * i;
+  }
+
+  Integral<MOD> operator () (int a, int b) const {
+    if (a < b || b < 0) return 0;
+    if (a < factor.size()) return factor[a] * inv_factor[b] * inv_factor[a - b];
+    b = std::min(b, a - b);
+    Integral<MOD> ret = 1;
+    for (int i = 1; i <= b; ++i) ret = ret * (a + 1 - i) / i;
+    return ret;
+  }
+};
+
+void test_binomial() {
+  const int MOD = 998244353;
+  using Binom = Binomial<MOD>;
+  Binom binom;
+  binom.init(100);
+  CHECK(1 == binom(99, 0).val());
+  CHECK(0 == binom(99, 10000).val());
+  CHECK(4950 == binom(100, 2).val());
+  CHECK(19900 == binom(200, 2).val());
+  CHECK(binom(200000, 2) == binom(200000, 199998));
+}
+
+int main() {
+  test_binomial();
+}
