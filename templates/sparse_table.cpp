5ccadbb34395cb004a76505be58ba16ccf071ad7
nero <286830682@qq.com>
2021-01-02 02:18:28 +0800

    Upgrade to C++17

--- templates/sparse_table.cpp
+++ templates/sparse_table.cpp
@@ -1,10 +1,8 @@
-template<typename T>
+template<typename T, bool kUseMax>
 struct SparseTable {
   std::vector<std::vector<T>> u;
   std::vector<int> lg;
-  std::function<T(T, T)> f;
-  void build(const std::vector<T>& a, const std::function<T(T, T)>& func) {
-    f = func;
+  void build(const std::vector<T>& a) {
     int n = a.size(), L = 1;
     while ((1 << L) <= n) ++L;
     std::vector<std::vector<T>>(n, std::vector<T>(L)).swap(u);
@@ -14,16 +12,24 @@ struct SparseTable {
     for (int i = 0; i < n; ++ i) u[i][0] = a[i];
     for (int j = 1; (1 << j) <= n; ++ j) {
       for (int i = 0; i + (1 << j) <= n; ++ i) {
-        u[i][j] = f(u[i][j - 1], u[i + (1 << (j - 1))][j - 1]);
+        if constexpr(kUseMax) {
+          u[i][j] = std::max(u[i][j - 1], u[i + (1 << (j - 1))][j - 1]);
+        } else {
+          u[i][j] = std::min(u[i][j - 1], u[i + (1 << (j - 1))][j - 1]);
+        }
       }
     }
   }
   T ask(int a, int b) {
     if (a > b) std::swap(a, b);
     int k = lg[b - a + 1];
-    return f(u[a][k], u[b - (1 << k) + 1][k]);
+    if constexpr(kUseMax) {
+      return std::max(u[a][k], u[b - (1 << k) + 1][k]);
+    } else {
+      return std::max(u[a][k], u[b - (1 << k) + 1][k]);
+    }
   }
 };
 
-SparseTable<int> st;
+SparseTable<int, true> st;
 
