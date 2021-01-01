61710df1bc95ef022721d0e74d928832948d622b
nero <286830682@qq.com>
2021-01-02 00:18:57 +0800

    Pass comparator to build

--- templates/sparse_table.cpp
+++ templates/sparse_table.cpp
@@ -2,7 +2,9 @@ template<typename T>
 struct SparseTable {
   std::vector<std::vector<T>> u;
   std::vector<int> lg;
-  void build(const std::vector<T>& a) {
+  std::function<T(T, T)> f;
+  void build(const std::vector<T>& a, const std::function<T(T, T)>& func) {
+    f = func;
     int n = a.size(), L = 1;
     while ((1 << L) <= n) ++L;
     std::vector<std::vector<T>>(n, std::vector<T>(L)).swap(u);
@@ -12,14 +14,14 @@ struct SparseTable {
     for (int i = 0; i < n; ++ i) u[i][0] = a[i];
     for (int j = 1; (1 << j) <= n; ++ j) {
       for (int i = 0; i + (1 << j) <= n; ++ i) {
-        u[i][j] = std::max(u[i][j - 1], u[i + (1 << (j - 1))][j - 1]);
+        u[i][j] = f(u[i][j - 1], u[i + (1 << (j - 1))][j - 1]);
       }
     }
   }
   T ask(int a, int b) {
     if (a > b) std::swap(a, b);
     int k = lg[b - a + 1];
-    return std::max(u[a][k], u[b - (1 << k) + 1][k]);
+    return f(u[a][k], u[b - (1 << k) + 1][k]);
   }
 };
 
