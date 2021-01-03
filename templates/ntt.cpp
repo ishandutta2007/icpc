392ca305347c6190983758bb230c8c1b03d535ef
nero <286830682@qq.com>
2021-01-03 13:17:10 +0800

    code format

--- templates/ntt.cpp
+++ templates/ntt.cpp
@@ -1,16 +1,15 @@
-// Require modulo_integral.cpp
-void ntt(Mint A[], int n, int inv) {
+template<int MOD = 998244353, int kPrimRoot = 3>
+void ntt(Integral<MOD> A[], int n, int inv) {
   // inv == 1: ntt, == -1: intt
 
-  const int ort = 3;  // primitive root of MOD. 3 is of 998244353.
   // MOD == a * b ^ k + 1, n <= b ^ k.
   // 998244353 == (7 * 17) * 2 ^ 23 + 1.
   // This code works only when b == 2.
-  Mint w = 1, d = Mint(ort).power((MOD - 1) / n), t;
+  Integral<MOD> w = 1, d = Integral<MOD>(kPrimRoot).power((MOD - 1) / n), t;
   int i, j, c, s;
   if (inv == -1) {
     for (i = 1, j = n - 1; i < j; ++i, --j) std::swap(A[i], A[j]);
-    for (t = Mint(n).inv(), i = 0; i < n; ++i) A[i] = A[i] * t;
+    for (t = Integral<MOD>(n).inv(), i = 0; i < n; ++i) A[i] = A[i] * t;
   }
   for (s = n >> 1; s; s >>= 1, w = 1, d = d * d) {
     for (c = 0; c < s; ++c, w = w * d) {
