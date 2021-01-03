39b48a321a109d4a3241d46ff85bee440c26a891
nero <286830682@qq.com>
2021-01-03 13:07:08 +0800

    NTT uses Integral<MOD>

--- templates/ntt.cpp
+++ templates/ntt.cpp
@@ -1,29 +1,22 @@
-using LL = long long;
-const int ort = 3;  // primitive root of MOD.
-const int MOD = 998244353;
-// MOD == a * b ^ k + 1, n <= b ^ k.
-// 998244353 == (7 * 17) * 2 ^ 23 + 1.
-// This code works only when b == 2.
-
-LL power(LL a, LL b, LL p) {
-  LL ret = 1;
-  for ( ; b; b >>= 1, a = a * a % p) if (b & 1) ret = ret * a % p;
-  return ret;
-}
-
-void ntt(LL A[], int n, int inv) {
+// Require modulo_integral.cpp
+void ntt(Mint A[], int n, int inv) {
   // inv == 1: ntt, == -1: intt
-  LL w = 1, d = power(ort, (MOD - 1) / n, MOD), t;
+
+  const int ort = 3;  // primitive root of MOD. 3 is of 998244353.
+  // MOD == a * b ^ k + 1, n <= b ^ k.
+  // 998244353 == (7 * 17) * 2 ^ 23 + 1.
+  // This code works only when b == 2.
+  Mint w = 1, d = Mint(ort).power((MOD - 1) / n), t;
   int i, j, c, s;
   if (inv == -1) {
     for (i = 1, j = n - 1; i < j; ++i, --j) std::swap(A[i], A[j]);
-    for (t = power(n, MOD - 2, MOD), i = 0; i < n; ++i) A[i] = A[i] * t % MOD;
+    for (t = Mint(n).inv(), i = 0; i < n; ++i) A[i] = A[i] * t;
   }
-  for (s = n >> 1; s; s >>= w = 1, d = d * d % MOD) {
-    for (c = 0; c < s; ++c, w = w * d % MOD) {
+  for (s = n >> 1; s; s >>= 1, w = 1, d = d * d) {
+    for (c = 0; c < s; ++c, w = w * d) {
       for (i = c; i < n; i += s << 1) {
-        A[i | s] = (A[i] + MOD - (t = A[i | s])) * w % MOD;
-        A[i] = (A[i] + t) % MOD;
+        A[i | s] = (A[i] - (t = A[i | s])) * w;
+        A[i] += t;
       }
     }
   }
@@ -32,4 +25,3 @@ void ntt(LL A[], int n, int inv) {
     if (i < j) std::swap(A[i], A[j]);
   }
 }
-
