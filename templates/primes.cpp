dff7c75d124789e1450473042f393fcbe24dac5c
nero <286830682@qq.com>
2021-01-18 22:29:05 +0800

    indent

--- templates/primes.cpp
+++ templates/primes.cpp
@@ -3,20 +3,20 @@ std::vector<int> mu;
 std::vector<int> min_div;
 
 void prepare_primes(int n) {
-	min_div.resize(n + 1);
-	mu.resize(n + 1);
-	min_div[1] = mu[1] = 1;
-	for (int i = 2; i <= n; ++i) {
-		if (!min_div[i]) {
-			primes.emplace_back(i);
-			min_div[i] = i;
-			mu[i] = -1;
-		}
-		for (int p : primes) {
-			if (i * p > n) break;
-			min_div[i * p] = p;
-			if (i % p == 0) break;
-			else mu[i * p] = -mu[i];
-		}
-	}
+  min_div.resize(n + 1);
+  mu.resize(n + 1);
+  min_div[1] = mu[1] = 1;
+  for (int i = 2; i <= n; ++i) {
+    if (!min_div[i]) {
+      primes.emplace_back(i);
+      min_div[i] = i;
+      mu[i] = -1;
+    }
+    for (int p : primes) {
+      if (i * p > n) break;
+      min_div[i * p] = p;
+      if (i % p == 0) break;
+      else mu[i * p] = -mu[i];
+    }
+  }
 }
