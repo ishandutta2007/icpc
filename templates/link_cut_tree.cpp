48925a2bedda60956b831e4b5c89d8a3add1aac8
nero <286830682@qq.com>
2021-02-18 17:55:22 +0800

    codeformat

--- templates/link_cut_tree.cpp
+++ templates/link_cut_tree.cpp
@@ -7,35 +7,33 @@
 struct Node *nill;
 
 struct Node {
-  Node *fa,*ch[2];
-  bool rev_tag;
-  int val,vmax;
-  int sz,vc;
+  Node* fa = nill;
+  Node* ch[2] = {nill, nill};
+  bool rev_tag = false;
+  int val = 0, vmax = 0;
+  int sz = 1, vc = 0;
   // sz   : splay子树的节点以及虚树节点和.
   // vc   : 虚树节点和.
   // vmax : splay子树最大val.
 
   //      : 所有修改操作只关于access后的根进行.
 
-  Node(int _val = 0) {
-    rev_tag = false;
-    fa = ch[0] = ch[1] = nill;
-    val = vmax = _val;
-    sz = 1;
-    vc = 0;
-  }
+  Node() {}
+  explicit Node(int val) : val(val), vmax(val) {}
 
   void up() {
-    if (this == nill) return ;
-    vmax = std::max(val,std::max(ch[0]->vmax,ch[1]->vmax));
+    if (this == nill) return;
+    vmax = std::max(val, std::max(ch[0]->vmax, ch[1]->vmax));
     sz = 1 + vc + ch[0]->sz + ch[1]->sz;
   }
 
   void down() {
-    if (this == nill || !rev_tag) return ;
-    rev_tag = false;
-    ch[0]->rev();
-    ch[1]->rev();
+    if (this == nill) return;
+    if (rev_tag) {
+      rev_tag = false;
+      ch[0]->rev();
+      ch[1]->rev();
+    }
   }
 
   void rev() {
@@ -43,41 +41,24 @@ struct Node {
     rev_tag ^= 1;
   }
 
-  bool d() {
-    return fa->ch[1] == this;
-  }
-
-  bool isroot() {
-    return fa == nill || fa->ch[0] != this && fa->ch[1] != this;
-  }
-
-  void setc(Node *p,int c) {
-    ch[c] = p;
-    p->fa = this;
-    up();
-  }
+  bool d() const { return fa->ch[1] == this; }
+  bool isroot() const { return fa == nill || (fa->ch[0] != this && fa->ch[1] != this); }
+  void D() { if (!isroot()) fa->D(); down(); }
+  void setc(Node *p,int c) { ch[c] = p; p->fa = this; up(); }
 
   void rot() {
     Node *f = fa,*ff = fa->fa;
-    int c = d(),cc = fa->d();
-    f->setc(ch[c ^ 1],c);
-    this->setc(f,c ^ 1);
-    if (ff->ch[cc] == f) ff->setc(this,cc);
+    int c = d(), cc = fa->d();
+    f->setc(ch[c ^ 1], c);
+    this->setc(f, c ^ 1);
+    if (ff->ch[cc] == f) ff->setc(this, cc);
     else this->fa = ff;
   }
 
-  void D() {
-    if (!isroot()) fa->D();
-    down();
-  }
-
   Node *splay() {
-    D();
-    while (!isroot()) {
-      if (!fa->isroot()) {
+    for (D(); !isroot(); rot()) {
+      if (!fa->isroot())
         d() == fa->d() ? fa->rot() : rot();
-      }
-      rot();
     }
     return this;
   }
@@ -123,8 +104,8 @@ struct Node {
   }
 };
 
-const int kN = 400000 + 5;
-Node pool[kN],*node[kN],*alloc;
+const int kN = 600000 + 5;
+Node pool[kN], *node[kN], *alloc;
 int n;
 
 void lct_init() {
@@ -133,7 +114,7 @@ void lct_init() {
   nill->fa = nill->ch[0] = nill->ch[1] = nill;
   nill->sz = 0;
 
-  for (int i = 1; i <= n; ++ i) {
+  for (int i = 0; i < n; ++ i) {
     node[i] = new(alloc ++) Node();
   }
 }
