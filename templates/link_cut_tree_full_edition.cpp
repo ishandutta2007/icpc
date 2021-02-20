9729c87b369fc4de308f016b0e1f9d67aa6ac34f
nero <286830682@qq.com>
2021-02-20 12:38:08 +0800

    Update

--- templates/link_cut_tree_full_edition.cpp
+++ templates/link_cut_tree_full_edition.cpp
@@ -6,53 +6,35 @@
 #define CHECK(...) (__VA_ARGS__)
 #endif
 
-// FIXME: Under construction.
 struct Node *nill;
 
+// Note: All modifications should act only on the root of the splay tree after access().
 struct Node {
+  // Fundamental fields for splay/LCT.
   Node* fa = nill;
   Node* ch[2] = {nill, nill};
   bool rev_tag = false;
 
-  struct Summary {
-    int val = 0;
+  // Node's own information.
+  int val = 0;
 
-    int sz = 1;  // sum of subtree size of virtual children plus sum of sz in the splay tree.
-    int vmax = 0;  // max val among val in the splay tree.
+  // Auxiliary summary.
+  int vmax = 0;  // the max val among splay nodes'.
+  int sz = 1;  // sum of sz of splay subtrees plus sum of sz of virtual subtrees.
 
-    Summary() {}
-    explicit Summary(int val) : val(val), vmax(val) {}
-  };
-
+  // Virtual children's summary.
   struct VirtualSummary {
-    int vc = 0;  // total size in virtual children's subtrees.
-
-    void add(const Summary& A) {
-      vc += A.sz;
-    }
-
-    void subtract(const Summary& A) {
-      vc -= A.sz;
-    }
+    int vc = 0;  // sum of sz of virtual subtrees.
   };
-
-  void recalculate(Summary& A,
-                   const VirtualSummary& O,
-                   const Summary& left,
-                   const Summary& right) {
-    A.sz = 1 + O.vc + left.sz + right.sz;
-    A.vmax = std::max(A.val, std::max(left.vmax, right.vmax));
-  }
-
-  Summary summary;
   VirtualSummary virtual_summary;
 
   Node() {}
-  explicit Node(int val) : summary(val) {}
+  explicit Node(int val) : val(val), vmax(val) {}
 
   void up() {
     if (this == nill) return;
-    recalculate(summary, virtual_summary, ch[0]->summary, ch[1]->summary);
+    vmax = std::max(val, std::max(ch[0]->vmax, ch[1]->vmax));
+    sz = 1 + virtual_summary.vc + ch[0]->sz + ch[1]->sz;
   }
 
   void down() {
@@ -69,9 +51,21 @@ struct Node {
     rev_tag ^= 1;
   }
 
+  void join_as_virtual_child(VirtualSummary& virtual_summary) const {
+    if (this == nill) return;
+    CHECK(is_splay_root());
+    virtual_summary.vc += this->sz;
+  }
+
+  void detach_from_virtual_child(VirtualSummary& virtual_summary) const {
+    if (this == nill) return;
+    CHECK(is_splay_root());
+    virtual_summary.vc -= this->sz;
+  }
+
   bool d() const { return fa->ch[1] == this; }
-  bool isroot() const { return fa == nill || (fa->ch[0] != this && fa->ch[1] != this); }
-  void D() { if (!isroot()) fa->D(); down(); }
+  bool is_splay_root() const { return fa == nill || (fa->ch[0] != this && fa->ch[1] != this); }
+  void D() { if (!is_splay_root()) fa->D(); down(); }
   void setc(Node *p, int c) { ch[c] = p; p->fa = this; up(); }
 
   void rot() {
@@ -84,8 +78,8 @@ struct Node {
   }
 
   Node* splay() {
-    for (D(); !isroot(); rot()) {
-      if (!fa->isroot())
+    for (D(); !is_splay_root(); rot()) {
+      if (!fa->is_splay_root())
         d() == fa->d() ? fa->rot() : rot();
     }
     return this;
@@ -94,8 +88,8 @@ struct Node {
   Node* access() {
     for (Node *p = this, *q = nill; p != nill; ) {
       p->splay();
-      p->virtual_summary.subtract(q->summary);
-      p->virtual_summary.add(p->ch[1]->summary);
+      p->ch[1]->join_as_virtual_child(p->virtual_summary);
+      q->detach_from_virtual_child(p->virtual_summary);
       p->setc(q,1);
       q = p;
       p = p->fa;
@@ -103,11 +97,19 @@ struct Node {
     return splay();
   }
 
-  void link(Node* p) {
-    p->access();
-    make_root()->fa = p;
-    p->virtual_summary.add(this->summary);
-    p->up();
+  void link(Node* parent) {
+    parent->access();
+    make_root()->fa = parent;
+    join_as_virtual_child(parent->virtual_summary);
+    parent->up();
+  }
+
+  void cut() {
+    access();
+    Node* parent = get_splay_precursor()->access();
+    detach_from_virtual_child(parent->virtual_summary);
+    fa = nill;
+    parent->up();
   }
 
   Node* make_root() {
@@ -116,14 +118,26 @@ struct Node {
     return this;
   }
 
+  Node* get_splay_precursor() {
+    this->down();
+    Node* p = this->ch[0];
+    CHECK(p != nill);
+    while (true) {
+      p->down();
+      if (p->ch[1] == nill) break;
+      p = p->ch[1];
+    }
+    return p;
+  }
+
   Node* get_max() {
     Node* p = this;
     while (true) {
       p->down();
-      if (p->summary.val >= p->ch[0]->summary.vmax && p->summary.val >= p->ch[1]->summary.vmax) {
+      if (p->val >= p->ch[0]->vmax && p->val >= p->ch[1]->vmax) {
         return p;
       }
-      if (p->ch[0]->summary.vmax >= p->ch[1]->summary.vmax) {
+      if (p->ch[0]->vmax >= p->ch[1]->vmax) {
         p = p->ch[0];
       } else {
         p = p->ch[1];
@@ -132,7 +146,7 @@ struct Node {
   }
 };
 
-const int kN = 600000 + 5;
+const int kN = 300000 + 5;
 Node pool[kN], *node[kN], *alloc;
 int n;
 
@@ -140,15 +154,9 @@ void lct_init() {
   alloc = pool;
   nill = new(alloc ++) Node();
   nill->fa = nill->ch[0] = nill->ch[1] = nill;
-  nill->summary.sz = 0;
+  nill->sz = 0;
 
   for (int i = 0; i < n; ++ i) {
     node[i] = new(alloc ++) Node();
   }
 }
-int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  std::istream& reader = std::cin;
-}
-
