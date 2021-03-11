ea788fdc3165d21de461d66fdef2b64c4954c2da
nero <286830682@qq.com>
2021-03-12 00:52:12 +0800

    Update diff.py

--- tools/diff.py
+++ tools/diff.py
@@ -1,13 +1,19 @@
 import filecmp
 import os
 
+os.environ['GEN'] = 'gen'
+os.environ['TEST'] = 'A'
+os.environ['BASELINE'] = 'A_good'
+
+os.system('echo ${GEN} ${TEST} ${BASELINE}')
+
 ca = 0
 while True:
-    os.system('./gen >F.in')
-    os.system('./F <F.in >F.out')
-    os.system('./F_good <F.in >F_good.out')
+    os.system('./${GEN} >${TEST}.in')
+    os.system('./${TEST} <${TEST}.in >test.out')
+    os.system('./${BASELINE} <${TEST}.in >baseline.out')
 
     ca += 1
     print("case:", ca)
-    if not filecmp.cmp('./F.out', './F_good.out'):
+    if not filecmp.cmp('./test.out', './baseline.out'):
         break
