c681d627fdf62deb7d27d440e9dd776ca41ccc8f
Nero <286830682@qq.com>
2021-05-04 11:02:07 +0800

    format spfa_cost_flow.cpp

--- templates/spfa_cost_flow.cpp
+++ templates/spfa_cost_flow.cpp
@@ -1,45 +1,50 @@
-template<int kN,typename T>
+template<typename T, int kN, int kE = 600000>
 struct CostFlow {
-  int s,t,head[kN],etot,prevv[kN],preve[kN],inq[kN],que[kN],qf,qe;
+  const T inf = std::numeric_limits<T>::infinity();
+  int s, t, head[kN], etot, prevv[kN], preve[kN];
   T dis[kN];
-  struct Edge {int v,next; T cap,cost;} g[501000];
+  struct Edge {int v, next; T cap, cost;} g[kE];
   void init() {
-    memset(head,-1,sizeof(head)); etot = 0;
+    memset(head, -1, sizeof(head)); etot = 0;
   }
-  void add_edge(int u,int v,T cap,T cost) {
-    g[etot].v = v; g[etot].cap = cap; g[etot].cost = cost; g[etot].next = head[u]; head[u] = etot ++;
-    g[etot].v = u; g[etot].cap = 0; g[etot].cost = -cost; g[etot].next = head[v]; head[v] = etot ++;
+  void add_edge(int u, int v, T cap, T cost) {
+    g[etot].v = v; g[etot].cap = cap; g[etot].cost = cost; g[etot].next = head[u]; head[u] = etot++;
+    g[etot].v = u; g[etot].cap = 0; g[etot].cost = -cost; g[etot].next = head[v]; head[v] = etot++;
   }
-  void mcmf(int _s,int _t,T &cost,T &flow) {
-    s = _s; t = _t; cost = flow = 0;
-    while (true) {
-      for (int i = 0; i < kN; i ++) dis[i] = (T)1e30;
-      dis[s] = 0;
-      qf = qe = 0;
-      que[qe++] = s;
-      while (qf!=qe) {
-        int u = que[qf++]; inq[u] = 0; if (qf==kN) qf = 0;
-        for (int i = head[u]; i != -1; i = g[i].next) {
-          Edge &e = g[i];
-          if (e.cap && dis[e.v]>dis[u]+e.cost) {
-            dis[e.v] = dis[u]+e.cost;
-            prevv[e.v] = u; preve[e.v] = i;
-            if (!inq[e.v]) {
-              que[qe++] = e.v;
-              if (qe==kN) qe = 0;
-              inq[e.v] = 1;
-            }
+  void spfa(int source) {
+    static int inq[kN], que[kN], qf, qe;
+    for (int i = 0; i < kN; i ++) dis[i] = inf;
+    dis[source] = 0;
+    qf = qe = 0;
+    que[qe++] = source;
+    while (qf != qe) {
+      int u = que[qf++]; inq[u] = 0; if (qf == kN) qf = 0;
+      for (int i = head[u]; i != -1; i = g[i].next) {
+        Edge &e = g[i];
+        if (e.cap && dis[e.v] > dis[u] + e.cost) {
+          dis[e.v] = dis[u] + e.cost;
+          prevv[e.v] = u; preve[e.v] = i;
+          if (!inq[e.v]) {
+            que[qe++] = e.v;
+            if (qe == kN) qe = 0;
+            inq[e.v] = 1;
           }
         }
       }
-      if (dis[t]==T(1e30)) break;
-      T f = (T)1e30;
+    }
+  }
+  void mcmf(int _s, int _t, T& cost, T& flow) {
+    s = _s; t = _t; cost = flow = 0;
+    while (true) {
+      spfa(s);
+      if (dis[t] == inf) break;
+      T f = inf;
       for (int u = t; u != s; u = prevv[u])
-        f = std::min(f,g[preve[u]].cap);
-      cost += f*dis[t];
+        f = std::min(f, g[preve[u]].cap);
+      cost += f * dis[t];
       flow += f;
       for (int u = t; u != s; u = prevv[u])
-        g[preve[u]].cap -= f,g[preve[u]^1].cap += f;
+        g[preve[u]].cap -= f, g[preve[u] ^ 1].cap += f;
     }
   }
 };
