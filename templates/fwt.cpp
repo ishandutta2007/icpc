4612fd2f552af4ab2f12211cadbd219a518f580f
Nero <286830682@qq.com>
2021-06-30 01:21:56 +0800

    Update fwt

--- templates/fwt.cpp
+++ templates/fwt.cpp
@@ -3,17 +3,18 @@ const int inv2 = MOD + 1 >> 1;
 int A[1 << 18];
 
 void fwt(int A[],int n,int inv) {
+  // [x^k]fwt(x^{a_i}) = (-1)^{__builtin_popcount(k & a_i)}
   for (int l = 1; l << 1 <= n; l <<= 1) {
     for (int s = 0; s < n; ++ s) {
       if (~s & l) {
         int a = A[s];
         int b = A[s ^ l];
         if (inv == 1) {
-          A[s] = (a - b + MOD) % MOD;
-          A[s ^ l] = (a + b) % MOD;
+          A[s] = (a + b) % MOD;
+          A[s ^ l] = (a - b + MOD) % MOD;
         } else {
           A[s] = (a + b) * 1ll * inv2 % MOD;
-          A[s ^ l] = (b - a + MOD) * 1ll * inv2 % MOD;
+          A[s ^ l] = (a - b + MOD) * 1ll * inv2 % MOD;
         }
       }
     }
