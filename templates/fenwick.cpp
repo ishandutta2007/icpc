40e7e33d639f6dacd4fc47165f79bb9a33cdda3c
Nero <286830682@qq.com>
2021-09-03 22:51:20 +0800

    Add fenwick

---
+++ templates/fenwick.cpp
@@ -0,0 +1,38 @@
+template<typename T>
+class Fenwick {
+ public:
+  Fenwick() {}
+
+  template<typename... Args>
+  explicit Fenwick(int n) : bits(n) {}
+
+  template<typename... Args>
+  explicit Fenwick(int n, Args... args) : bits(n, T(args...)) {}
+
+  template<typename... Args>
+  void update(int p, const T& val) {
+    for (int i = p; i < bits.size(); i += ~i & i + 1) bits[i] += val;
+  }
+
+  template<typename... Args>
+  void update(int p, Args... args) {
+    for (int i = p; i < bits.size(); i += ~i & i + 1) bits[i].update(args...);
+  }
+
+  template<typename U, typename... Args>
+  U query(int p) const {
+    U ret{};
+    for (int i = p; i >= 0; i -= ~i & i + 1) ret += bits[i];
+    return ret;
+  }
+
+  template<typename U, typename... Args>
+  U query(int p, Args... args) const {
+    U ret{};
+    for (int i = p; i >= 0; i -= ~i & i + 1) ret += bits[i].template query<U>(args...);
+    return ret;
+  }
+
+ private:
+  std::vector<T> bits;
+};
