1faac927effb5826adeed58cd3772132bc39b844
ssstare <286830682@qq.com>
2021-09-07 09:54:52 +0800

    Add dsu.cpp

---
+++ templates/dsu.cpp
@@ -0,0 +1,26 @@
+struct Dsu {
+  int n;
+  std::vector<int> dsu;
+
+  explicit Dsu(int n) : n(n), dsu(n) {
+    std::iota(dsu.begin(), dsu.end(), 0);
+  }
+
+  int find(int x) {
+    return dsu[x] == x ? x : dsu[x] = find(dsu[x]);
+  }
+
+  bool is_connected(int x, int y) {
+    x = find(x);
+    y = find(y);
+    return x == y;
+  }
+
+  bool merge(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) return false;
+    dsu[x] = y;
+    return true;
+  }
+};
