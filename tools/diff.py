607eb96e4ca01a0f03cf52136b090d24699c47fb
ssstare <286830682@qq.com>
2021-10-09 20:28:46 +0800

    Fix

--- tools/diff.py
+++ tools/diff.py
@@ -12,14 +12,14 @@ def main():
 @click.option('--binary', required=True)
 @click.option('--gen', required=True)
 @click.option('--num-iteration', default=100)
-@click.option('--time-interval', default=None)
+@click.option('--time-interval', default=0.0)
 def run(binary, gen, num_iteration, time_interval):
     for _ in range(num_iteration):
         with open('data.in', 'w') as f:
             subprocess.run(gen, stdout=f, check=True)
         with open('data.in', 'r') as f:
             subprocess.run(binary, stdin=f, check=True)
-        if time_interval:
+        if time_interval > 0.0:
             time.sleep(time_interval)
 
 if __name__ == '__main__':
