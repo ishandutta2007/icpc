a2a726bb9c2fa01ba315ee3bc0e992e29890d9da
ssstare <286830682@qq.com>
2021-10-09 20:53:33 +0800

    Support --good

--- tools/diff.py
+++ tools/diff.py
@@ -2,6 +2,7 @@ import click
 import logging
 import subprocess
 import time
+import filecmp
 
 @click.group()
 def main():
@@ -9,16 +10,25 @@ def main():
     logger.setLevel(logging.INFO)
 
 @main.command()
-@click.option('--binary', required=True)
 @click.option('--gen', required=True)
+@click.option('--binary', required=True)
+@click.option('--good', default=None)
 @click.option('--num-iteration', default=100)
 @click.option('--time-interval', default=0.0)
-def run(binary, gen, num_iteration, time_interval):
+def run(gen, binary, good, num_iteration, time_interval):
     for _ in range(num_iteration):
         with open('data.in', 'w') as f:
             subprocess.run(gen, stdout=f, check=True)
         with open('data.in', 'r') as f:
-            subprocess.run(binary, stdin=f, check=True)
+            with open('data.out', 'w') as g:
+                subprocess.run(binary, stdin=f, stdout=g, check=True)
+        if good:
+            with open('data.in', 'r') as f:
+                with open('good.out', 'w') as g:
+                    subprocess.run(good, stdin=f, stdout=g, check=True)
+                if not filecmp.cmp('data.out', 'good.out'):
+                    logging.ERROR('Catch!!!')
+                    return
         if time_interval > 0.0:
             time.sleep(time_interval)
 
