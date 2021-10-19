9bed1323e076f220d147e6573826b0cad029b694
ssstare <286830682@qq.com>
2021-10-19 11:34:47 +0800

    Add revertable_dsu.cpp

---
+++ templates/revertable_dsu.cpp
@@ -0,0 +1,41 @@
+struct RevertableDsu {
+  std::vector<int> dsu;
+  std::vector<int> sz;
+
+  explicit RevertableDsu(int n) : dsu(n), sz(n, 1) {
+    std::iota(dsu.begin(), dsu.end(), 0);
+  }
+
+  int find(int x) {
+    while (x != dsu[x]) x = dsu[x];
+    return x;
+  }
+
+  bool merge(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) return false;
+    if (sz[x] < sz[y]) std::swap(x, y);
+    modify(sz[x], sz[x] + sz[y]);
+    modify(dsu[y], x);
+    return true;
+  }
+
+  int history_size() const { return history.size(); }
+
+  void revert(int h) {
+    while (history.size() > h) {
+      history.back().first = history.back().second;
+      history.pop_back();
+    }
+  }
+
+ private:
+  void modify(int& a, int b) {
+    history.emplace_back(a, a);
+    a = b;
+  }
+
+  std::vector<std::pair<int&, int>> history;
+};
+
