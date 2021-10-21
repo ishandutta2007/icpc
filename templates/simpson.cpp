8fa3eb61d36ba2c41fc5a1ab6f18d24e1a0d908a
ssstare <286830682@qq.com>
2021-10-21 17:19:27 +0800

    Format simpson.cpp

--- templates/simpson.cpp
+++ templates/simpson.cpp
@@ -1,19 +1,27 @@
-double F(double x) {
-  // 待积分函数
-  return sqrt(1 + 4 * a * a * x * x);
-}
-double simpson(double a,double b) {
-  double c = (a + b) * .5;
-  return (F(a) + 4 * F(c) + F(b)) * (b - a) / 6;
-}
-double asr(double a,double b,double eps,double A) {
-  double c = (a + b) * .5;
-  double L = simpson(a,c),R = simpson(c,b);
-  if (std::abs(L + R - A) <= 15 * eps) {
-    return L + R + (L + R - A) / 15;
+struct Simpson {
+ public:
+  // f is the function to be integrated.
+  // F :: double -> double
+  // e.g. double f(double x) { return std::sqrt(1 + 4 * a * a * x * x); }
+  template<typename F>
+  static double calc(double L, double R, F&& f) {
+    return asr(L, R, 1e-5, simpson(L, R, std::forward<F>(f)), std::forward<F>(f));
   }
-  return asr(a,c,eps / 2,L) + asr(c,b,eps / 2,R);
-}
-double calc(double L,double R) {
-  return asr(L,R,1e-5,simpson(L,R));
-}
+
+ private:
+  template<typename F>
+  static double simpson(double a, double b, F&& f) {
+    double c = (a + b) * .5;
+    return (f(a) + 4 * f(c) + f(b)) * (b - a) / 6;
+  }
+  template<typename F>
+  static double asr(double a, double b, double eps, double A, F&& f) {
+    double c = (a + b) * .5;
+    double L = simpson(a, c, std::forward<F>(f)), R = simpson(c, b, std::forward<F>(f));
+    if (std::abs(L + R - A) <= 15 * eps) {
+      return L + R + (L + R - A) / 15;
+    }
+    return asr(a, c, eps / 2, L, std::forward<F>(f)) + asr(c, b, eps / 2, R, std::forward<F>(f));
+  }
+};
+
