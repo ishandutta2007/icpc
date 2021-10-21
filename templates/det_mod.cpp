01581ab5376e57ab936df152de7a148a9aab8958
ssstare <286830682@qq.com>
2021-10-21 16:02:09 +0800

    Update det_mod.cpp

--- templates/det_mod.cpp
+++ templates/det_mod.cpp
@@ -1,17 +1,14 @@
-const int MOD = (int)1e9 + 7;
-
-inline void add(int &a,int b) {
-  a += b;
-  if (a >= MOD) a -= MOD;
-}
-int det(int A[16][16],int n) {
-  int ret = 1;
+template<int MOD, template<int> typename Integral>
+Integral<MOD> det(std::vector<std::vector<Integral<MOD>>> A) {
+  if (A.empty() || A[0].size() != A.size()) return 0;
+  Integral<MOD> ret = 1;
+  const int n = A.size();
   for (int i = 0; i < n; ++ i) {
     if (A[i][i] == 0) {
       for (int j = i + 1; j < n; ++ j) {
-        if (A[j][i]) {
+        if (A[j][i].val()) {
           for (int k = i; k < n; ++ k) {
-            std::swap(A[i][k],A[j][k]);
+            std::swap(A[i][k], A[j][k]);
           }
           ret = -ret;
           break;
@@ -20,29 +17,27 @@ int det(int A[16][16],int n) {
       if (A[i][i] == 0) return 0;
     }
     for (int j = i + 1; j < n; ++ j) {
-      int a = 1,b = 0,c = 0,d = 1;
-      int x = A[i][i],y = A[j][i];
+      Integral<MOD> a = 1, b = 0, c = 0, d = 1;
+      int x = A[i][i].val(), y = A[j][i].val();
       while (y) {
         int t = x / y;
         if (t < 0) t += MOD;
-        add(a,MOD - c * 1ll * t % MOD);
-        add(b,MOD - d * 1ll * t % MOD);
-        std::swap(a,c);
-        std::swap(b,d);
+        a -= c * t;
+        b -= d * t;
+        std::swap(a, c);
+        std::swap(b, d);
         x %= y;
-        std::swap(x,y);
+        std::swap(x, y);
         ret = -ret;
       }
       for (int k = 0; k < n; ++ k) {
-        int q = A[i][k],w = A[j][k];
-        A[i][k] = (a * 1ll * q + b * 1ll * w) % MOD;
-        A[j][k] = (c * 1ll * q + d * 1ll * w) % MOD;
+        Integral<MOD> q = A[i][k], w = A[j][k];
+        A[i][k] = a * q + b * w;
+        A[j][k] = c * q + d * w;
       }
     }
-    ret = A[i][i] * 1ll * ret % MOD;
-  }
-  if (ret < 0) {
-    ret += MOD;
+    ret *= A[i][i];
   }
   return ret;
 }
+
