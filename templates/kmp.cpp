7a57c0740599df40be975d5430449826e1a7043c
ssstare <286830682@qq.com>
2021-10-21 16:39:54 +0800

    Format kmp.cpp

--- templates/kmp.cpp
+++ templates/kmp.cpp
@@ -1,24 +1,30 @@
-std::vector<int> prepare(const std::string& w) {
-  std::vector<int> next(w.size());
-  next[0] = -1;
-  for (int i = 1, j = -1; i < w.size(); ++i) {
-    while (j >= 0 && w[i] != w[j + 1]) j = next[j];
-    next[i] = w[i] == w[j + 1] ? ++j : -1;
-  }
-  return next;
-}
+template<typename String>
+struct KnuthMorrisPratt {
+  const String& str;
+  std::vector<int> next;
 
-int count_occurrence(const std::string& s, const std::string& w, const std::vector<int>& next) {
-  int ret = 0;
-  for (int i = 0, j = -1; i < s.length(); ++i) {
-    while (j >= 0 && s[i] != w[j + 1]) j = next[j];
-    if (s[i] == w[j + 1]) {
-      ++j;
-      if (j + 1 == w.length()) {
-        j = next[j];
-        ++ret;
-      }
+  explicit KnuthMorrisPratt(const String& str) : str(str), next(str.size()) {
+    if (str.size() == 0) return;
+    next[0] = -1;
+    for (int i = 1, j = -1; i < str.size(); ++i) {
+      while (j >= 0 && str[i] != str[j + 1]) j = next[j];
+      next[i] = str[i] == str[j + 1] ? ++j : -1;
     }
   }
-  return ret;
-}
+
+  int count_occurrence(const String& s) const {
+    int ret = 0;
+    for (int i = 0, j = -1; i < s.length(); ++i) {
+      while (j >= 0 && s[i] != str[j + 1]) j = next[j];
+      if (s[i] == str[j + 1]) {
+        ++j;
+        if (j + 1 == str.length()) {
+          j = next[j];
+          ++ret;
+        }
+      }
+    }
+    return ret;
+  }
+};
+
