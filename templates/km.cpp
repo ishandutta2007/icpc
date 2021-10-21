a575d4750faf592efa4bcbc4b12b7be4f21da227
ssstare <286830682@qq.com>
2021-10-21 16:14:10 +0800

    Format km.cpp

--- templates/km.cpp
+++ templates/km.cpp
@@ -1,28 +1,32 @@
-template<typename T, int kN>
-struct KM {
-  int n, m, lv[kN], rv[kN], left[kN];
-  T G[kN][kN], la[kN], ra[kN];
-  const T inf = std::numeric_limits<T>::infinity();
+template<typename T>
+struct KuhnMunkres {
+  static constexpr T inf = std::numeric_limits<T>::max();
+  int n = 0, m = 0;
+  std::vector<int> lv, rv, left;
+  std::vector<std::vector<T>> adj;
+  std::vector<T> la, ra;
+
+  KuhnMunkres(int n, int m) : n(n), m(m), lv(n), rv(m), left(m), adj(n, std::vector<T>(m, -inf)), la(n), ra(m) {}
+
   bool expath(int u) {
     lv[u] = 1;
     for (int i = 0; i < m; ++i)
-      if (!rv[i] && la[u] + ra[i] == G[u][i]) {
+      if (!rv[i] && la[u] + ra[i] == adj[u][i]) {
         rv[i] = 1;
         if (left[i] == -1 || expath(left[i]))
           return left[i] = u, true;
       }
     return false;
   }
-  void init() {
-    for (int i = 0; i < kN; ++i)
-      for (int j = 0; j < kN; ++j) G[i][j] = -inf;
-  }
-  int km(int _n,int _m) {
-    n = _n; m = _m;
-    memset(left, -1, sizeof(left));
+  int km(int n, int m) {
+    assert(this->n >= n);
+    assert(this->m >= m);
+    this->n = n;
+    this->m = m;
+    std::fill(left.begin(), left.end(), -1);
     for (int i = 0; i < n; ++i) {
       la[i] = -inf;
-      for (int j = 0; j < m; ++j) la[i] = std::max(la[i], G[i][j]);
+      for (int j = 0; j < m; ++j) la[i] = std::max(la[i], adj[i][j]);
     }
     for (int i = 0; i < m; ++i) ra[i] = 0;
     for (int u = 0; u < n; ++u) {
@@ -32,7 +36,7 @@ struct KM {
         T d = inf;
         for (int i = 0; i < n; ++i) if (lv[i])
           for (int j = 0; j < m; ++j) if (!rv[j])
-            d = std::min(d, la[i] + ra[j] - G[i][j]);
+            d = std::min(d, la[i] + ra[j] - adj[i][j]);
         for (int i = 0; i < n; ++i)
           if (lv[i]) la[i] -= d, lv[i] = 0;
         for (int i = 0; i < m; ++i)
@@ -40,7 +44,7 @@ struct KM {
       }
     }
     int ret = 0;
-    for (int i = 0; i < m; ++i) if (left[i] != -1) ret += G[left[i]][i];
+    for (int i = 0; i < m; ++i) if (left[i] != -1) ret += adj[left[i]][i];
     return ret;
   }
 };
