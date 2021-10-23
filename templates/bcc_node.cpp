96a323a5f9b8536d9ccd11fc3dc60bb963082c61
ssstare <286830682@qq.com>
2021-10-23 11:42:49 +0800

    Format bcc_node.cpp

--- templates/bcc_node.cpp
+++ templates/bcc_node.cpp
@@ -1,102 +1,85 @@
-const int kN = 10000 + 5;
-const int kM = 100000 + 5;
+struct VertexBiconnectedComponent {
+  int n = 0, m = 0;
+  std::vector<int> dfn, low;  // Note: Unusual dfn.
+  std::vector<int> head, belong;
+  std::vector<bool> is_cut, visited;
+  std::vector<int> stack;
+  int btot = 0;
 
-int dfn[kN],low[kN],head[kN],etot,btot,n,m,nq,belong[kN];
-bool is_cut[kN],visited[kN];
-std::stack<int> stack;
-struct Edge {
-  int v,next,belong;
-  bool visited,is_cut;
-}g[kM<<1];
+  struct Edge {
+    int v = 0, next = -1, belong = -1;
+    bool visited = false, is_cut = false;
+  };
+  std::vector<Edge> g;
 
-std::vector<int> graph[kN+kM];
+  std::vector<std::vector<int>> shrinked_graph;
 
-void add_edge(int u,int v) {
-  g[etot].belong = -1; g[etot].visited = g[etot].is_cut = false;
-  g[etot].v = v; g[etot].next = head[u]; head[u] = etot ++;
-}
+  VertexBiconnectedComponent() = default;
+  VertexBiconnectedComponent(int n, int m) : n(n), m(m), dfn(n, -1), low(n), head(n, -1), belong(n, -1), is_cut(n), visited(n) {}
 
-void tarjan(int u,int root,int tim) {
-  dfn[u] = low[u] = tim;
-  visited[u] = true;
-  int child_count = 0;
-  for (int i = head[u]; i != -1; i = g[i].next) {
-    Edge &e = g[i];
-    if (e.visited) continue;
-    stack.push(i);
-    g[i].visited = g[i^1].visited = true;
-    if (visited[e.v]) {
-      low[u] = std::min(low[u],dfn[e.v]);
-      continue;
-    }
-    tarjan(e.v,root,tim+1);
-    g[i].is_cut = g[i^1].is_cut = (low[e.v]>dfn[u] || g[i].is_cut);
-    if (u!=root) is_cut[u] |= (low[e.v]>=dfn[u]);
-    if (low[e.v]>=dfn[u] || u==root) {
-      while (true) {
-        int id = stack.top(); stack.pop();
-        g[id].belong = g[id^1].belong = btot;
-        if (id==i) break;
+  void add_edge(int u, int v) {
+    g.emplace_back(Edge{.v = v, .next = head[u]});
+    head[u] = (int)g.size() - 1;
+  }
+
+  void tarjan(int u, int root, int tim) {
+    dfn[u] = low[u] = tim;
+    visited[u] = true;
+    int child_count = 0;
+    for (int i = head[u]; i != -1; i = g[i].next) {
+      Edge &e = g[i];
+      if (e.visited) continue;
+      stack.emplace_back(i);
+      g[i].visited = g[i ^ 1].visited = true;
+      if (visited[e.v]) {
+        low[u] = std::min(low[u], dfn[e.v]);
+        continue;
       }
-      btot ++;
+      tarjan(e.v, root, tim + 1);
+      g[i].is_cut = g[i ^ 1].is_cut = (low[e.v] > dfn[u] || g[i].is_cut);
+      if (u != root) is_cut[u] = is_cut[u] || (low[e.v] >= dfn[u]);
+      if (low[e.v] >= dfn[u] || u == root) {
+        while (true) {
+          int id = stack.back(); stack.pop_back();
+          g[id].belong = g[id ^ 1].belong = btot;
+          if (id == i) break;
+        }
+        btot++;
+      }
+      low[u] = std::min(low[e.v], low[u]);
+      child_count++;
     }
-    low[u] = std::min(low[e.v],low[u]);
-    child_count ++;
+    if (u == root && child_count > 1) is_cut[u] = true;
   }
-  if (u==root && child_count>1) is_cut[u] = true;
-}
 
-void bcc() {
-  for (int i = 0; i < n; ++ i) {
-    dfn[i] = 0;
-    is_cut[i] = false;
-    visited[i] = false;
+  void bcc() {
+    for (int i = 0; i < n; ++i) if (!visited[i]) tarjan(i, i, 0);
   }
-  btot = 0;
-  for (int i = 0; i < n; ++ i) {
-    if (!visited[i]) {
-      tarjan(i,i,1);
-    }
-  }
-}
 
-void build() {
-  std::fill(graph,graph+n+m,std::vector<int>());
-  for (int u = 0; u < n; ++ u) {
-    if (is_cut[u] || head[u]==-1) {
-      int id = btot ++;
-      belong[u] = id;
-      for (int i = head[u]; i != -1; i = g[i].next) {
-        Edge &e = g[i];
-        int v = e.belong;
-        graph[id].push_back(v);
-        graph[v].push_back(id);
+  void build() {
+    for (int u = 0; u < n; ++u) {
+      if (is_cut[u] || head[u] == -1) {
+        int id = btot++;
+        shrinked_graph.emplace_back();
+        belong[u] = id;
+        for (int i = head[u]; i != -1; i = g[i].next) {
+          Edge &e = g[i];
+          int v = e.belong;
+          shrinked_graph[id].push_back(v);
+          shrinked_graph[v].push_back(id);
+        }
       }
     }
-  }
-  for (int u = 0; u < btot; ++ u) {
-    std::sort(graph[u].begin(),graph[u].end());
-    graph[u].erase(std::unique(graph[u].begin(),graph[u].end()),graph[u].end());
-  }
-  for (int i = 0; i < m; ++ i) {
-    int u = g[i<<1].v;
-    int v = g[i<<1|1].v;
-    if (!is_cut[u]) belong[u] = g[i<<1].belong;
-    if (!is_cut[v]) belong[v] = g[i<<1].belong;
-  }
-}
-
-int main() {
-  while (scanf("%d%d",&n,&m)==2) {
-    std::fill(head,head+n,-1); etot = 0;
-    for (int i = 0; i < m; ++ i) {
-      int a,b;
-      scanf("%d%d",&a,&b); a --; b --;
-      add_edge(a,b);
-      add_edge(b,a);
+    for (int u = 0; u < btot; ++u) {
+      std::sort(shrinked_graph[u].begin(), shrinked_graph[u].end());
+      shrinked_graph[u].erase(std::unique(shrinked_graph[u].begin(), shrinked_graph[u].end()), shrinked_graph[u].end());
     }
-    bcc();
-    build();
+    for (int i = 0; i < m; ++i) {
+      int u = g[i << 1].v;
+      int v = g[i << 1 | 1].v;
+      if (!is_cut[u]) belong[u] = g[i << 1].belong;
+      if (!is_cut[v]) belong[v] = g[i << 1].belong;
+    }
   }
-  return 0;
-}
+};
+
