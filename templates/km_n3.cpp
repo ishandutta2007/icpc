61b831000539b78c0579b2887ea02a10afe515f7
ssstare <286830682@qq.com>
2021-10-23 23:40:35 +0800

    Format

--- templates/km_n3.cpp
+++ templates/km_n3.cpp
@@ -1,28 +1,30 @@
-const int INF = 0x3f3f3f3f;
-
-int km(int n, int m, const std::vector<std::vector<int>>& graph) {
+template<typename T>
+std::pair<T, std::vector<int>> km(int n, int m, const std::vector<std::vector<T>>& adj) {
   // Returns the MINIMUM perfect matching.
   // Index: 1..n, 1..m
-  std::vector<int> u(n + 1), v(m + 1), p(m + 1), way(m + 1);
+  const T inf = std::numeric_limits<T>::max();
+  std::vector<T> u(n + 1), v(m + 1);
+  std::vector<int> p(m + 1), way(m + 1);
   for (int i = 1; i <= n; ++ i) {
     p[0] = i;
     int j0 = 0;
-    std::vector<int> minv(m + 1, INF);
+    std::vector<T> minv(m + 1, inf);
     std::vector<bool> used(m + 1,false);
     do {
       used[j0] = true;
-      int i0 = p[j0],delta = INF,j1;
+      int i0 = p[j0], j1;
+      T delta = inf;
       for (int j = 1; j <= m; ++ j)
         if (!used[j]) {
-          int cur = graph[i0][j] - u[i0] - v[j];
+          T cur = adj[i0][j] - u[i0] - v[j];
           if (cur < minv[j])
-            minv[j] = cur,way[j] = j0;
+            minv[j] = cur, way[j] = j0;
           if (minv[j] < delta)
-            delta = minv[j],j1 = j;
+            delta = minv[j], j1 = j;
         }
       for (int j = 0; j <= m; ++j)
         if (used[j])
-          u[p[j]] += delta,v[j] -= delta;
+          u[p[j]] += delta, v[j] -= delta;
         else
           minv[j] -= delta;
       j0 = j1;
@@ -33,6 +35,6 @@ int km(int n, int m, const std::vector<std::vector<int>>& graph) {
       j0 = j1;
     } while (j0);
   }
-  return -v[0];
+  return std::make_pair(-v[0], p);
 }
 
