47e0ea2edcb27a9211b019e0ad1040d2b0769e33
ssstare <286830682@qq.com>
2021-11-01 11:37:06 +0800

    Add sieve for phi

--- templates/primes.cpp
+++ templates/primes.cpp
@@ -2,6 +2,7 @@ struct PrimeTable {
   std::vector<int> primes;
   std::vector<int> min_div;
   std::vector<int8_t> mu;
+  std::vector<int> phi;
 
   PrimeTable() = default;
   explicit PrimeTable(int n) { prepare_primes(n); }
@@ -10,18 +11,25 @@ struct PrimeTable {
     primes.clear();
     min_div.resize(n + 1);
     mu.resize(n + 1);
-    min_div[1] = mu[1] = 1;
+    phi.resize(n + 1);
+    min_div[1] = mu[1] = phi[1] = 1;
     for (int i = 2; i <= n; ++i) {
       if (!min_div[i]) {
         primes.emplace_back(i);
         min_div[i] = i;
         mu[i] = -1;
+        phi[i] = i - 1;
       }
       for (int p : primes) {
         if (i * p > n) break;
         min_div[i * p] = p;
-        if (i % p == 0) break;
-        else mu[i * p] = -mu[i];
+        if (i % p == 0) {
+          phi[i * p] = phi[i] * p;
+          break;
+        } else {
+          mu[i * p] = -mu[i];
+          phi[i * p] = phi[i] * (p - 1);
+        }
       }
     }
   }
