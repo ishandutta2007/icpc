e5f0fee76974674b504fb3c73a4afb3157e31106
ssstare <286830682@qq.com>
2021-11-23 18:19:21 +0800

    Format suffix_automaton.cpp

--- templates/suffix_automaton.cpp
+++ templates/suffix_automaton.cpp
@@ -1,37 +1,61 @@
-const int kN = 20000 + 5;
+struct SuffixAutomaton {
+  static constexpr int kCharsetSize = 26;
+  static int hash(char c) { return c - 'a'; }
 
-struct kNode {
-  kNode *fa,*go[26];
-  int val;
-  kNode(int _val = 0) : fa(0),val(_val) {
-    memset(go,0,sizeof(go));
+  explicit SuffixAutomaton(int length = 0) : pool(length * 2 + 1), alloc(pool.data()) {
+    root = last = new(alloc++) Node(0);
   }
-};
-kNode *root,*last,pool[kN],*bat;
+  explicit SuffixAutomaton(const std::string& str) : SuffixAutomaton(str.length()) {
+    for (int i = 0; i < str.length(); ++i) extend(hash(str[i]));
+  }
 
-void extend(int c) {
-  kNode *p = last;
-  kNode *np = new(bat++) kNode(p->val+1);
-  while (p && p->go[c] == 0) {
-    p->go[c] = np; p = p->fa;
-  }
-  if (p == 0) {
-    np->fa = root;
-  } else {
-    kNode *q = p->go[c];
-    if (p->val+1 == q->val) {
-      np->fa = q;
+  struct Node {
+    Node* fa = nullptr;
+    std::array<Node*, kCharsetSize> go = {nullptr};
+    int val = 0;
+
+    explicit Node(int val = 0) : val(val) {}
+  };
+
+  std::vector<Node> pool;
+  Node* alloc = nullptr;
+  Node* root = nullptr;
+  Node* last = nullptr;
+
+  void extend(int c) {
+    Node* p = last;
+    Node* np = new(alloc++) Node(p->val + 1);
+    while (p && p->go[c] == 0) {
+      p->go[c] = np; p = p->fa;
+    }
+    if (p == 0) {
+      np->fa = root;
     } else {
-      kNode *nq = new(bat++) kNode(p->val+1);
-      memcpy(nq->go,q->go,sizeof(q->go));
-      nq->fa = q->fa;
-      q->fa = nq;
-      np->fa = nq;
-      while (p && p->go[c] == q) {
-        p->go[c] = nq;
-        p = p->fa;
+      Node* q = p->go[c];
+      if (p->val + 1 == q->val) {
+        np->fa = q;
+      } else {
+        Node* nq = new(alloc++) Node(p->val + 1);
+        std::copy(q->go.begin(), q->go.end(), nq->go.begin());
+        nq->fa = q->fa;
+        q->fa = nq;
+        np->fa = nq;
+        while (p && p->go[c] == q) {
+          p->go[c] = nq;
+          p = p->fa;
+        }
       }
     }
+    last = np;
   }
-  last = np;
-}
+
+  bool is_substr(const std::string& str) const {
+    Node* u = root;
+    for (char c : str) {
+      u = u->go[hash(c)];
+      if (u == nullptr) return false;
+    }
+    return true;
+  }
+};
+
