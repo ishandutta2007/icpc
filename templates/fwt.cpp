cbe79bf045f89c1cb4c4cd4470533eb8bae907b7
ssstare <286830682@qq.com>
2021-11-29 11:54:28 +0800

    Format fwt.cpp

--- templates/fwt.cpp
+++ templates/fwt.cpp
@@ -1,22 +1,25 @@
-const int MOD = (int)1e9 + 7;
-const int inv2 = MOD + 1 >> 1;
-int A[1 << 18];
-
-void fwt(int A[],int n,int inv) {
+// https://en.wikipedia.org/wiki/Fast_Walsh%E2%80%93Hadamard_transform
+template<typename T>
+static void fwt(T* A, int n, int inv) {
   // [x^k]fwt(x^{a_i}) = (-1)^{__builtin_popcount(k & a_i)}
+  //
+  // n should be a power of 2.
+  // inv == 1: forward; inv == -1: reverse.
+  const T inv2 = T(1) / 2;
   for (int l = 1; l << 1 <= n; l <<= 1) {
     for (int s = 0; s < n; ++ s) {
       if (~s & l) {
-        int a = A[s];
-        int b = A[s ^ l];
+        T a = A[s];
+        T b = A[s ^ l];
         if (inv == 1) {
-          A[s] = (a + b) % MOD;
-          A[s ^ l] = (a - b + MOD) % MOD;
+          A[s] = a + b;
+          A[s ^ l] = a - b;
         } else {
-          A[s] = (a + b) * 1ll * inv2 % MOD;
-          A[s ^ l] = (a - b + MOD) * 1ll * inv2 % MOD;
+          A[s] = (a + b) * inv2;
+          A[s ^ l] = (a - b) * inv2;
         }
       }
     }
   }
 }
+
