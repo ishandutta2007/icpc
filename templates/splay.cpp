0d15c7192d2caf95d6c7e4c7bf1028f2a59bf353
ssstare <286830682@qq.com>
2021-12-01 12:15:12 +0800

    Rename to Splay

--- templates/splay.cpp
+++ templates/splay.cpp
@@ -1,16 +1,16 @@
-struct Node* nill = nullptr;
+struct Splay* nill = nullptr;
 
-struct Node {
-  Node* fa = nill;
-  Node* ch[2] = {nill, nill};
+struct Splay {
+  Splay* fa = nill;
+  Splay* ch[2] = {nill, nill};
   bool reverse_tag = false;
 
   int sz = 1;
 
   static void initialize() {
     if (nill == nullptr) {
-      static Node nill_storage;
-      nill = new(&nill_storage) Node();
+      static Splay nill_storage;
+      nill = new(&nill_storage) Splay();
       nill->sz = 0;
     }
   }
@@ -35,18 +35,18 @@ struct Node {
   }
 
   bool d() const { return fa->ch[1]==this; }
-  void setc(Node* o, int c) { ch[c] = o; o->fa = this; up(); }
+  void setc(Splay* o, int c) { ch[c] = o; o->fa = this; up(); }
   void rot() {
     int c = d(), cc = fa->d();
-    Node* z = fa->fa;
-    Node* tmp = fa;
+    Splay* z = fa->fa;
+    Splay* tmp = fa;
     fa->setc(ch[c ^ 1], c);
     setc(tmp, c ^ 1);
     z->setc(this, cc);
   }
   void D() { if (fa != nill) fa->D(); down(); }
 
-  Node* splay(Node* aim = nill) {
+  Splay* splay(Splay* aim = nill) {
     for (D(); fa != aim; rot()) {
       if (fa->fa != aim) {
         d() == fa->d() ? fa->rot() : rot();
