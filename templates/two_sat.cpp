6f84b6ad485c173037e46ee0fcc0c53ce90ed0eb
ssstare <286830682@qq.com>
2021-12-02 14:31:17 +0800

    Format 2-sat

--- templates/two_sat.cpp
+++ templates/two_sat.cpp
@@ -1,36 +1,49 @@
-template<int kN>
-struct Sat {
-  int head[kN],etot,stack[kN],top;
-  bool mark[kN];
-  struct Edge {int v,next;} g[501000];
-  void init() {
-    memset(head,-1,sizeof(head)); etot = 0;
+struct TwoSat {
+ public:
+  int n;
+  std::vector<int> head;
+  std::vector<int> stack;
+  std::vector<bool> mark;
+
+  struct Edge {
+    int v, next;
+  };
+  std::vector<Edge> g;
+
+  explicit TwoSat(int n) : n(n), head(n, -1), mark(n, false) {}
+
+  void add_edge(int u, int v) {
+    g.emplace_back(Edge{.v = v, .next = head[u]});
+    head[u] = (int)g.size() - 1;
   }
-  void addEdge(int u,int v) {
-    g[etot].v = v; g[etot].next = head[u]; head[u] = etot ++;
+
+  bool solve() {
+    for (int i = 0; i < n; i += 2) {
+      if (!mark[i] && !mark[i + 1]) {
+        stack.clear();
+        if (!dfs(i)) {
+          while (!stack.empty()) {
+            int v = stack.back(); stack.pop_back();
+            mark[v] = false;
+          }
+          if (!dfs(i + 1)) return false;
+        }
+      }
+    }
+    return true;
   }
+
+ private:
   bool dfs(int u) {
-    if (mark[u^1]) return false;
+    if (mark[u ^ 1]) return false;
     if (mark[u]) return true;
     mark[u] = true;
-    stack[top++] = u;
+    stack.emplace_back(u);
     for (int i = head[u]; i != -1; i = g[i].next) {
       int v = g[i].v;
       if (!dfs(v)) return false;
     }
     return true;
   }
-  bool work(int n) {
-    memset(mark,0,sizeof(mark));
-    for (int i = 0; i < n; i += 2) {
-      if (!mark[i] && !mark[i+1]) {
-        top = 0;
-        if (!dfs(i)) {
-          while (top) mark[stack[--top]] = false;
-          if (!dfs(i+1)) return false;
-        }
-      }
-    }
-    return true;
-  }
 };
+
