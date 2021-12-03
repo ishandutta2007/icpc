7b9ceda096b95800ac7214dea6cd59f205cb40c8
ssstare <286830682@qq.com>
2021-12-03 12:15:01 +0800

    Format dmst with path

--- templates/minimum_directed_tree_with_path.cpp
+++ templates/minimum_directed_tree_with_path.cpp
@@ -1,152 +1,176 @@
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+#ifdef ALGO
+#include "el_psy_congroo.hpp"
+#else
+#define DUMP(...)
+#define CHECK(...)
+#endif
 
-#define Clr(a,b) memset(a,b,sizeof(a))
+namespace {
 
-using namespace std;
-
-const int MAXN = 100010;
-
-struct COST;
-vector<COST*> cov;
-
-struct COST {
-  int c, id, used;
-  COST * a, * b; // c = a - b;
-
-  bool operator < (const COST & t) const {
-    return c < t.c;
+struct DirectedMinimumSpanningTree {
+ public:
+  void add_edge(int u, int v, int cost, int id) {
+    g.emplace_back(Edge{.u = u, .v = v, .c = create_cost(cost, id), .from = u, .to = v, .val = cost});
   }
-  COST(int c, int id):c(c),id(id),a(0),b(0),used(0) {
-    cov.push_back(this);
-  }
-  COST(COST * a, COST * b):a(a),b(b),c(a->c-b->c),id(-1),used(0) {
-    cov.push_back(this);
-  }
-  void push() {
-    if(id == -1) {
-      a -> used += used;
-      b -> used -= used;
+
+  int solve(int root, int n, int m, std::vector<int>* path = nullptr) {
+    int i, j, k, cnt, id;
+
+    std::vector<int> vis(n);
+    std::vector<Cost*> least(n);
+    std::vector<int> in(n);  // pre
+    std::vector<int> idx(n);
+
+    int ret = 0;
+    while(true) {
+      vis.resize(n);
+      least.assign(n, nullptr);
+      in.assign(n, -1);
+      idx.resize(n);
+      for(i = 0; i < m; ++i) {
+        int u = g[i].u;
+        int v = g[i].v;
+        if(u != v && cmp(g[i].c, least[v])) {
+          least[v] = g[i].c;
+          in[v] = u;
+        }
+      }
+      for(i = 0; i < n; ++i) if(i != root && in[i] == -1) return -1;
+      std::fill(vis.begin(), vis.end(), -1);
+      std::fill(idx.begin(), idx.end(), -1);
+      for(i = cnt = id = 0, in[root] = -1; i < n; ++i) if(i != root) {
+        ret += least[i]->c;
+        least[i]->use();
+        if(~vis[i]) continue;
+        for(j = i, k = -1; ; j = in[j]) {
+          if(~vis[j]) {
+            if(vis[j] == cnt) k = j;
+            break;
+          }
+          vis[j] = cnt;
+        }
+        if(~k) {
+          for(j = in[k]; j != k; j = in[j]) idx[j] = id;
+          idx[k] = id++;
+        }
+        ++cnt;
+      }
+      if(!id) break;
+      // Relabeling.
+      for(i = 0; i < n; ++i) if(idx[i] == -1) idx[i] = id++;
+      for(i = 0; i < m; ++i) {
+        int v = g[i].v;
+        g[i].u = idx[g[i].u];
+        g[i].v = idx[g[i].v];
+        if(g[i].u != g[i].v)
+          g[i].c = create_cost(g[i].c, least[v]);
+      }
+      n = id;
+      root = idx[root];
     }
+    if (path != nullptr) {
+      path->clear();
+      for(int i = (int)cov.size() - 1; i >= 0; --i) cov[i]->push();
+      for(int i = 0; i < cov.size(); ++i) {
+        Cost* c = cov[i];
+        if(c->id != -1 && c->c > 0 && c->used > 0) path->push_back(c->id);
+      }
+      sort(path->begin(), path->end());
+    }
+    return ret;
   }
-  void use() { ++used; }
+
+ private:
+  struct Cost {
+    int c, id;
+    int used = 0;
+    Cost* a = nullptr;
+    Cost* b = nullptr;  // c = a - b;
+
+    Cost(int c, int id) : c(c), id(id), a(nullptr), b(nullptr) {}
+    Cost(Cost* a, Cost* b) : a(a), b(b), c(a->c - b->c), id(-1) {}
+
+    bool operator < (const Cost& t) const { return c < t.c; }
+    void push() {
+      if(id == -1) {
+        a->used += used;
+        b->used -= used;
+      }
+    }
+
+    void use() { ++used; }
+  };
+
+  Cost* create_cost(int c, int id) {
+    cov_storage.emplace_back(new Cost(c, id));
+    cov.emplace_back(cov_storage.back().get());
+    return cov.back();
+  }
+
+  Cost* create_cost(Cost* a, Cost* b) {
+    cov_storage.emplace_back(new Cost(a, b));
+    cov.emplace_back(cov_storage.back().get());
+    return cov.back();
+  }
+
+  bool cmp(Cost* a, Cost* b) const {  // a < b
+    if(a == 0 || b == 0) return b == 0;
+    else return a->c < b->c;
+  }
+
+  bool better(Cost* a, Cost* b) const {  // a better than b
+    if (a == 0 || b == 0) return b == 0;
+    return a->c < b->c;
+  }
+
+  struct Edge {
+    int u, v;
+    Cost* c = nullptr;
+    int from, to, val;
+  };
+
+  std::vector<Edge> g;
+  std::vector<Cost*> cov;
+  std::vector<std::unique_ptr<Cost>> cov_storage;
 };
 
-struct EDGE {
-  int u, v;
-  COST * c;
+struct Solver {
 
-  int from, to, val;
-
-  EDGE(){}
-  EDGE(int u, int v, int cost, int id):u(u),v(v) {
-    c = new COST(cost, id);
-    from = u;
-    to = v;
-    val = cost;
+  void solve(int ca, std::istream& reader) {
+    int n, m;
+    scanf("%d%d", &n, &m);
+    DirectedMinimumSpanningTree dmst;
+    for(int i=0; i<m; i++) {
+      int x, y, z;
+      scanf("%d%d%d", &x, &y, &z);
+      --x, --y;
+      dmst.add_edge(x, y, z, i + 1);
+    }
+    std::vector<int> path;
+    int ans = dmst.solve(0, n, m, &path);
+    if(ans < 0) {
+      puts("-1");
+    } else {
+      printf("%d\n", ans);
+      for(int i = 0; i < path.size(); ++i) printf("%d ", path[i]);
+      printf("\n");
+    }
   }
-}   edge[MAXN];
+};
 
-int edgel;
+}  // namespace
 
-inline bool cmp(COST * a, COST * b) { // a < b
-  if(a == 0 || b == 0) return b == 0;
-  else return a->c < b->c;
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::istream& reader = std::cin;
+
+  int cas = 1;
+  // reader >> cas;
+  for (int ca = 1; ca <= cas; ++ca) {
+    auto solver = std::make_unique<Solver>();
+    solver->solve(ca, reader);
+  }
 }
 
-bool better(COST *a, COST *b) { //a better than b?
-  if (a == 0 || b == 0)
-    return b == 0;
-  return a->c < b->c;
-}
-
-bool Directed_MST(int st, int nV, int nE, int &ret) {
-  int i, j, k, idx[MAXN], cnt, id, vis[MAXN];
-  int in[MAXN];//pre
-  COST * least[MAXN];
-
-  ret = 0;
-  while(true) {
-    for(i=0; i<nV; ++i) least[i] = NULL, in[i] = -1;
-    for(i=0; i<nE; ++i) {
-      int &u = edge[i].u, &v = edge[i].v;
-      if(u != v && cmp(edge[i].c, least[v])) {
-        least[v] = edge[i].c;
-        in[v] = u;
-      }
-    }
-    for(i=0; i<nV; ++i) if(i != st && in[i] == -1) return false;
-    Clr(vis, -1);
-    Clr(idx, -1);
-    for(i=cnt=id=0, in[st]=-1; i<nV; ++i) if(i!=st) {
-      ret += least[i]->c;
-      least[i] -> use();
-      if(~vis[i]) continue;
-      for(j=i, k=-1; ; j=in[j]) {
-        if(~vis[j]) {
-          if(vis[j] == cnt) k = j;
-          break;
-        }
-        vis[j] = cnt;
-      }
-      if(~k) {
-        for(j=in[k]; j!=k; j=in[j]) idx[j] = id;
-        idx[k] = id++;
-      }
-      ++cnt;
-    }
-    if(!id) break;
-    //重标号
-    for(i=0; i<nV; ++i) if(idx[i] == -1) idx[i] = id++;
-    for(i=0; i<nE; ++i) {
-      int v = edge[i].v;
-      edge[i].u = idx[edge[i].u];
-      edge[i].v = idx[edge[i].v];
-      if(edge[i].u != edge[i].v)
-        edge[i].c = new COST(edge[i].c, least[v]);
-    }
-    nV = id;
-    st = idx[st];
-  }
-  return true;
-}
-
-int main()
-{
-  //freopen("input.txt","r",stdin);
-  //freopen("output.txt","w",stdout);
-
-  int n, m;
-  scanf("%d%d", &n, &m);
-  edgel = 0;
-  for(int i=0; i<m; i++)
-  {
-    int x, y, z;
-    scanf("%d%d%d", &x, &y, &z);
-    --x, --y;
-    edge[edgel++] = EDGE(x, y, z, i+1);
-  }
-  int ans;
-  if(!Directed_MST(0, n, m, ans))
-    puts("-1");
-  else
-  {
-    printf("%d\n", ans);
-    for(int i=cov.size()-1; i>=0; --i)
-      cov[i] -> push();
-    vector<int> tv;
-    for(int i=0; i<cov.size(); ++i)
-    {
-      COST * c = cov[i];
-      if(c -> id != -1 && c -> c > 0 && c -> used > 0)
-        tv.push_back(c -> id);
-    }
-    sort(tv.begin(), tv.end());
-    for(int i=0; i<tv.size(); ++i) printf("%d ", tv[i]);
-    printf("\n");
-  }
-  return 0;
-}
