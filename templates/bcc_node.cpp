461d04be9ece0b753660e7ccca3103c8711fc1e2
ssstare <286830682@qq.com>
2021-12-17 14:21:24 +0800

    Update

--- templates/bcc_node.cpp
+++ templates/bcc_node.cpp
@@ -1,3 +1,31 @@
+template<typename Edge>
+struct ShrinkedGraph {
+  std::vector<std::vector<Edge>> graph;
+  std::vector<bool> is_cut;
+
+  explicit ShrinkedGraph(int n) : graph(n), is_cut(n) {}
+
+  int size() const { return graph.size(); }
+
+  int add_point() {
+    int u = graph.size();
+    graph.emplace_back();
+    is_cut.emplace_back();
+    return u;
+  }
+
+  void remove_redundant_edges() {
+    for (int u = 0; u < graph.size(); ++u) {
+      std::sort(graph[u].begin(), graph[u].end(), [](const Edge& lhs, const Edge& rhs) {
+        return lhs.v < rhs.v;
+      });
+      graph[u].erase(std::unique(graph[u].begin(), graph[u].end(), [](const Edge& lhs, const Edge& rhs) {
+        return lhs.v == rhs.v;
+      }), graph[u].end());
+    }
+  }
+};
+
 struct VertexBiconnectedComponent {
   int n = 0, m = 0;
   std::vector<int> dfn, low;  // Note: Unusual dfn.
@@ -11,8 +39,6 @@ struct VertexBiconnectedComponent {
     bool visited = false, is_cut = false;
   };
   std::vector<Edge> g;
-
-  std::vector<std::vector<int>> shrinked_graph;
 
   VertexBiconnectedComponent() = default;
   VertexBiconnectedComponent(int n, int m) : n(n), m(m), dfn(n, -1), low(n), head(n, -1), belong(n, -1), is_cut(n), visited(n) {}
@@ -58,30 +84,30 @@ struct VertexBiconnectedComponent {
     for (int i = 0; i < n; ++i) if (!visited[i]) tarjan(i, i, 0);
   }
 
-  void build() {
+  template<typename ShrinkedEdge>
+  ShrinkedGraph<ShrinkedEdge> build() {
+    ShrinkedGraph<ShrinkedEdge> shrinked_graph(btot);
     for (int u = 0; u < n; ++u) {
       if (is_cut[u] || head[u] == -1) {
-        int id = btot++;
-        shrinked_graph.emplace_back();
+        int id = shrinked_graph.add_point();
         belong[u] = id;
         for (int i = head[u]; i != -1; i = g[i].next) {
-          Edge &e = g[i];
+          const Edge &e = g[i];
           int v = e.belong;
-          shrinked_graph[id].push_back(v);
-          shrinked_graph[v].push_back(id);
+          shrinked_graph.graph[id].emplace_back(ShrinkedEdge{.v = v});
+          shrinked_graph.graph[v].emplace_back(ShrinkedEdge{.v = id});
         }
       }
     }
-    for (int u = 0; u < btot; ++u) {
-      std::sort(shrinked_graph[u].begin(), shrinked_graph[u].end());
-      shrinked_graph[u].erase(std::unique(shrinked_graph[u].begin(), shrinked_graph[u].end()), shrinked_graph[u].end());
-    }
+    btot = shrinked_graph.size();
+    shrinked_graph.remove_redundant_edges();
     for (int i = 0; i < m; ++i) {
       int u = g[i << 1].v;
       int v = g[i << 1 | 1].v;
-      if (!is_cut[u]) belong[u] = g[i << 1].belong;
-      if (!is_cut[v]) belong[v] = g[i << 1].belong;
+      if (!is_cut[u]) belong[u] = g[i << 1].belong; else shrinked_graph.is_cut[belong[u]] = true;
+      if (!is_cut[v]) belong[v] = g[i << 1].belong; else shrinked_graph.is_cut[belong[v]] = true;
     }
+    return shrinked_graph;
   }
 };
 
