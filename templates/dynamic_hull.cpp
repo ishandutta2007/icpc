4f0de60982e0d10cff30eac973da33fc76cb0431
Nero <286830682@qq.com>
2021-12-18 23:01:48 +0800

    archeology

--- templates/dynamic_hull.cpp
+++ templates/dynamic_hull.cpp
@@ -1,46 +1,38 @@
-using LL = long long;
-
-struct Line {
-  static constexpr LL kIsQuery = -(1LL << 62);
-  LL m, b;
-  mutable std::function<const Line*()> succ;
-  bool operator < (const Line &rhs) const {
-    if (rhs.b != kIsQuery) return m < rhs.m;
-    const Line* s = succ();
-    if (!s) return false;
-    LL x = rhs.m;
-    return b - s->b < (s->m - m) * x;  // ld?
-  }
-};
-
-std::string to_string(const Line& line) {
-  return "Line{" + std::to_string(line.m) + ", " + std::to_string(line.b) + "}";
-}
-
 struct DynamicHull {
-  // Two operation types are supported:
-  // - Insert a new line with slope equals to m and intercept equals to b.
-  // - Given a param x, find the maximum value of m * x + b among lines_.
  public:
-  DynamicHull() = default;
-  ~DynamicHull()  = default;
+  using LL = long long;
 
-  void insert_line(LL m, LL b) {
-    auto y = lines_.insert(Line{.m = m, .b = b});
-    y->succ = [=] { return std::next(y) == lines_.end() ? nullptr : &*std::next(y); };
-    if (bad(y)) {
-      lines_.erase(y);
-      return;
+  // Two operation types are supported:
+  // - Insert a new line with slope equals to k and intercept equals to m.
+  // - Given a param x, find the maximum value of k * x + m among lines_.
+  DynamicHull() = default;
+  ~DynamicHull() = default;
+
+  struct Line {
+    static constexpr LL inf = std::numeric_limits<LL>::max();
+    LL k, m;
+    mutable LL p;
+    Line(LL k, LL m, LL p) : k(k), m(m), p(p) {}
+    bool is_query() const { return m == inf; }
+    bool operator < (const Line &rhs) const {
+      if (is_query() || rhs.is_query()) return p < rhs.p;
+      return k < rhs.k;
     }
-    while (std::next(y) != lines_.end() && bad(std::next(y))) lines_.erase(std::next(y));
-    while (y != lines_.begin() && bad(std::prev(y))) lines_.erase(std::prev(y));
+  };
+
+  void insert_line(LL k, LL m) {
+    auto y = lines_.insert(Line(k, m, 0));
+    for (auto z = std::next(y); isect(y, z); z = lines_.erase(z));
+    if (y != lines_.begin()) {
+      auto x = std::prev(y);
+      if (isect(x, y)) isect(x, y = lines_.erase(y));
+      while ((y = x) != lines_.begin() && (--x)->p >= y->p) isect(x, lines_.erase(y));
+    }
   }
 
   LL eval(LL x) {
-    auto iter = lines_.lower_bound(Line{x, Line::kIsQuery});
-    assert(iter != lines_.end());
-    auto l = *iter;
-    return l.m * x + l.b;
+    auto line = *lines_.lower_bound(Line(0, Line::inf, x));
+    return line.k * x + line.m;
   }
 
   bool empty() const { return lines_.empty(); }
@@ -48,15 +40,16 @@ struct DynamicHull {
   const std::multiset<Line>& lines() const { return lines_; }
 
  private:
-  bool bad(std::multiset<Line>::iterator y) {
-    auto z = std::next(y);
-    if (y == lines_.begin()) {
-      if (z == lines_.end()) return false;
-      return y->m == z->m && y->b <= z->b;
-    }
-    auto x = std::prev(y);
-    if (z == lines_.end()) return y->m == x->m && y->b <= x->b;
-    return (x->b - y->b) * (z->m - y->m) >= (y->b - z->b) * (y->m - x->m);  // ld?
+  using iterator = std::multiset<Line>::iterator;
+
+  LL div(LL a, LL b) {
+    return a / b - ((a ^ b) < 0 && a % b);
+  }
+  bool isect(iterator a, iterator b) {
+    if (b == lines_.end()) { a->p = Line::inf; return false; }
+    if (a->k == b->k) a->p = a->m > b->m ? Line::inf : -Line::inf;
+    else a->p = div(b->m - a->m, a->k - b->k);
+    return a->p >= b->p;
   }
 
   std::multiset<Line> lines_;
