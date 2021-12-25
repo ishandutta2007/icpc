f42d2e26aa35e7cd46a2a5396f5f01878c6f536e
Nero <286830682@qq.com>
2021-12-25 12:42:25 +0800

    Log input files which has errors

--- tools/diff.py
+++ tools/diff.py
@@ -43,8 +43,10 @@ def run(gen, binary, good, num_iterations, time_interval):
 @click.option('--output-suffix', default='.out')
 def from_data(data_dir, binary, output_dir, input_suffix, output_suffix):
     pathlib.Path(output_dir).mkdir(exist_ok=True)
+    problematic_list = []
     for input_file in glob.glob(data_dir + "/*" + input_suffix):
         logging.info('Processing {}'.format(input_file))
+        ok = True
         with open(input_file, 'r') as f:
             expected_output_file = pathlib.PurePath(data_dir, pathlib.PurePath(input_file).stem + output_suffix)
             output_file = pathlib.PurePath(output_dir, pathlib.PurePath(input_file).stem + output_suffix)
@@ -53,8 +55,17 @@ def from_data(data_dir, binary, output_dir, input_suffix, output_suffix):
                     subprocess.run(binary, stdin=f, stdout=g, check=True)
                 if not filecmp.cmp(expected_output_file, output_file):
                     logging.error('Difference catched when processing {}.'.format(input_file))
+                    ok = False
             except:
                 logging.error('Processing {} fail, just skip'.format(input_file))
+                ok = False
+        if ok == False:
+            problematic_list.append(input_file)
+    if (len(problematic_list) > 0):
+        logging.error("problematic_list: {}".format(problematic_list))
+        for input_file in problematic_list:
+            logging.error("{}: file_size={}".format(input_file, pathlib.Path(input_file).stat().st_size))
+
 
 if __name__ == '__main__':
     main()
