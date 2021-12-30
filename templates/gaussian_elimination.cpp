219d1f6b31fe1440e695e2690491edb3593d5722
ssstare <286830682@qq.com>
2021-12-30 14:30:36 +0800

    Add get_determinant

--- templates/gaussian_elimination.cpp
+++ templates/gaussian_elimination.cpp
@@ -2,10 +2,11 @@
 template<typename T>
 struct GaussianElimination {
  public:
+  // Apply gaussian elimination to matrix A.
   static bool solve(std::vector<std::vector<T>>& A) {
     std::vector<T> extra_column(A.size());
-    // Always true.
-    return solve(A, extra_column, nullptr);
+    gaussian_elimination_internal(A, extra_column);
+    return true;
   }
 
   // Return true if the linear system is solvable.
@@ -15,37 +16,20 @@ struct GaussianElimination {
     if (num_rows == 0) return true;
     const int num_columns = A[0].size();
 
-    int i, j, row, col, bestr;
-    for (row = 0, col = 0; row < num_rows && col < num_columns; ++row, ++col) {
-      for (; col < num_columns; ++col) {
-        bestr = row;
-        for (i = row + 1; i < num_rows; ++i) {
-          if (better(A[i][col], A[bestr][col])) bestr = i;
-        }
-        if (better(A[bestr][col], 0)) break;
-      }
-      if (col == num_columns) break;
-      if (row != bestr) {
-        for (j = col; j < num_columns; ++j) std::swap(A[row][j], A[bestr][j]);
-        std::swap(extra_column[row], extra_column[bestr]);
-      }
-      extra_column[row] /= A[row][col];
-      for (j = col + 1; j < num_columns; ++j) A[row][j] /= A[row][col];
-      A[row][col] = T(1);
-      for (i = 0; i < num_rows; ++i) {
-        if (i == row || A[i][col] == T(0)) continue;
-        extra_column[i] -= extra_column[row] * A[i][col];
-        for (j = col + 1; j < num_columns; ++j) A[i][j] -= A[row][j] * A[i][col];
-        A[i][col] = 0;
+    T determinant = gaussian_elimination_internal(A, extra_column);
+
+    if (!better(determinant, 0)) {
+      for (int r = 0; r < num_rows; ++r) {
+        bool empty_row = true;
+        for (int c = 0; c < num_columns; ++c) empty_row &= !better(A[r][c], 0);
+        if (empty_row && better(extra_column[r], 0)) return false;
       }
     }
-    for (int r = row; r < num_rows; ++r) if (better(extra_column[r], 0)) return false;
     if (nullable_solution != nullptr) {
       nullable_solution->assign(num_columns, T(0));
-      for (int r = 0, c = 0; r < row; ++r) {
+      for (int r = 0, c = 0; r < num_rows && c < num_columns; ++r) {
         while (c < num_columns && !better(A[r][c], 0)) ++c;
-        assert(c < num_columns);
-        nullable_solution->at(c) = extra_column[r] / A[r][c];
+        if (c < num_columns) nullable_solution->at(c) = extra_column[r] / A[r][c];
       }
     }
     return true;
@@ -80,6 +64,11 @@ struct GaussianElimination {
     return true;
   }
 
+  static T get_determinant(std::vector<std::vector<T>> A) {
+    std::vector<T> extra_column(A.size(), T(0));
+    return gaussian_elimination_internal(A, extra_column);
+  }
+
  private:
   // Return true if x is better than y.
   static bool better(T x, T y) {
@@ -89,6 +78,45 @@ struct GaussianElimination {
     } else {
       return x != 0;
     }
+  }
+
+  // Eliminate the matrix A into upper triangular form.
+  // Return the determinant.
+  static T gaussian_elimination_internal(std::vector<std::vector<T>>& A, std::vector<T>& extra_column) {
+    const int num_rows = A.size();
+    assert(extra_column.size() == num_rows);
+    if (num_rows == 0) return 1;
+    const int num_columns = A[0].size();
+
+    T determinant = T(1);
+    int i, j, row, col, bestr;
+    for (row = 0, col = 0; row < num_rows && col < num_columns; ++row, ++col) {
+      for (; col < num_columns; ++col) {
+        bestr = row;
+        for (i = row + 1; i < num_rows; ++i) {
+          if (better(A[i][col], A[bestr][col])) bestr = i;
+        }
+        if (better(A[bestr][col], 0)) break;
+      }
+      if (col == num_columns) break;
+      if (row != bestr) {
+        for (j = col; j < num_columns; ++j) std::swap(A[row][j], A[bestr][j]);
+        std::swap(extra_column[row], extra_column[bestr]);
+        determinant *= -1;
+      }
+      extra_column[row] /= A[row][col];
+      for (j = col + 1; j < num_columns; ++j) A[row][j] /= A[row][col];
+      determinant *= A[row][col];
+      A[row][col] = T(1);
+      for (i = 0; i < num_rows; ++i) {
+        if (i == row || A[i][col] == T(0)) continue;
+        extra_column[i] -= extra_column[row] * A[i][col];
+        for (j = col + 1; j < num_columns; ++j) A[i][j] -= A[row][j] * A[i][col];
+        A[i][col] = 0;
+      }
+    }
+    if (row != num_rows) determinant = 0;
+    return determinant;
   }
 };
 
