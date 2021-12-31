030f0798555520f4181511f140c6d5237b69b38c
ssstare <286830682@qq.com>
2021-12-31 11:25:05 +0800

    Format

--- templates/bitset.cpp
+++ templates/bitset.cpp
@@ -1,28 +1,7 @@
-struct BitSet {
-  using ULL = unsigned long long;
-  int n = 0;
-  std::vector<ULL> buckets;
-
-  static int get_bucket_size(int n) { return ((n - 1) >> 6) + 1; }
-  static int lowbit(ULL val) { return __builtin_ffsll(val) - 1; }
-
-  BitSet() = default;
-  explicit BitSet(int n) : n(n), buckets(get_bucket_size(n)) { CHECK(n > 0); }
-
-  // [0, n)
-  bool in_range(int p) const { return 0 <= p && p < n; }
-
-  void align_last_bucket_of(int m) {
-    if (!(m & 63)) return;
-    buckets[get_bucket_size(m) - 1] &= ((1ULL << (m & 63)) - 1);
-  }
-  void align_last_bucket() { align_last_bucket_of(n); }
-  void resize(int m) {
-    CHECK(m > 0);
-    buckets.resize(get_bucket_size(m), 0);
-    if (m < n) align_last_bucket_of(m);
-    n = m;
-  }
+struct Bitset {
+ public:
+  Bitset() = default;
+  explicit Bitset(int n) : n(n), buckets(get_bucket_size(n)) { CHECK(n > 0); }
 
   void flip(int p) { CHECK(in_range(p)); buckets[p >> 6] ^= 1ULL << (p & 63); }
   void flip() {
@@ -52,30 +31,30 @@ struct BitSet {
     return -1;
   }
 
-  BitSet& operator&=(const BitSet& other) {
+  Bitset& operator&=(const Bitset& other) {
     resize(std::max(size(), other.size()));
     int m = other.size();
     for (int i = ((m - 1) >> 6) + 1; i < (int)buckets.size(); ++i) buckets[i] = 0;
     for (int i = (m - 1) >> 6; i >= 0; --i) buckets[i] &= other.buckets[i];
     return *this;
   }
-  BitSet operator&(const BitSet& other) const { BitSet ret = *this; return ret &= other; }
+  Bitset operator&(const Bitset& other) const { Bitset ret = *this; return ret &= other; }
 
-  BitSet& operator|=(const BitSet& other) {
+  Bitset& operator|=(const Bitset& other) {
     resize(std::max(size(), other.size()));
     for (int i = 0; i < other.buckets.size(); ++i) buckets[i] |= other.buckets[i];
     return *this;
   }
-  BitSet operator|(const BitSet& other) const { BitSet ret = *this; return ret |= other; }
+  Bitset operator|(const Bitset& other) const { Bitset ret = *this; return ret |= other; }
 
-  BitSet operator^=(const BitSet& other) {
+  Bitset operator^=(const Bitset& other) {
     resize(std::max(size(), other.size()));
     for (int i = 0; i < other.buckets.size(); ++i) buckets[i] ^= other.buckets[i];
     return *this;
   }
-  BitSet operator^(const BitSet& other) const { BitSet ret = *this; return ret ^= other; }
-  BitSet operator~() const { BitSet ret = *this; ret.flip(); return ret; }
-  BitSet& operator>>=(int m) {
+  Bitset operator^(const Bitset& other) const { Bitset ret = *this; return ret ^= other; }
+  Bitset operator~() const { Bitset ret = *this; ret.flip(); return ret; }
+  Bitset& operator>>=(int m) {
     CHECK(m >= 0);
     int p = m >> 6;
     int w = m & 63;
@@ -86,8 +65,8 @@ struct BitSet {
     }
     return *this;
   }
-  BitSet operator>>(int m) const { BitSet ret = *this; return ret >>= m; }
-  BitSet& operator<<=(int m) {
+  Bitset operator>>(int m) const { Bitset ret = *this; return ret >>= m; }
+  Bitset& operator<<=(int m) {
     CHECK(m >= 0);
     int p = m >> 6;
     int w = m & 63;
@@ -103,7 +82,7 @@ struct BitSet {
     align_last_bucket();
     return *this;
   }
-  BitSet operator<<(int m) const { BitSet ret = *this; return ret <<= m; }
+  Bitset operator<<(int m) const { Bitset ret = *this; return ret <<= m; }
 
   int touch(int l, int r) const {  // return lowbit in [l, r]
     int lat = l >> 6;
@@ -121,14 +100,42 @@ struct BitSet {
     }
     return -1;
   }
+
+  std::string to_string() const {
+    std::stringstream ss;
+    for (int i = (int)buckets.size() - 1; i >= 0; --i) {
+      ss << std::hex << buckets[i];
+      if (i) ss << "-";
+    }
+    return ss.str();
+  }
+
+ private:
+  using ULL = unsigned long long;
+
+  static int get_bucket_size(int n) { return ((n - 1) >> 6) + 1; }
+  static int lowbit(ULL val) { return __builtin_ffsll(val) - 1; }
+
+  // [0, n)
+  bool in_range(int p) const { return 0 <= p && p < n; }
+
+  void align_last_bucket_of(int m) {
+    if (!(m & 63)) return;
+    buckets[get_bucket_size(m) - 1] &= ((1ULL << (m & 63)) - 1);
+  }
+  void align_last_bucket() { align_last_bucket_of(n); }
+  void resize(int m) {
+    CHECK(m > 0);
+    buckets.resize(get_bucket_size(m), 0);
+    if (m < n) align_last_bucket_of(m);
+    n = m;
+  }
+
+  int n = 0;
+  std::vector<ULL> buckets;
 };
 
-std::string to_string(const BitSet& bits) {
-  std::stringstream ss;
-  for (int i = (int)bits.buckets.size() - 1; i >= 0; --i) {
-    ss << std::hex << bits.buckets[i];
-    if (i) ss << "-";
-  }
-  return ss.str();
+std::string to_string(const Bitset& bitset) {
+  return bitset.to_string();
 }
 
