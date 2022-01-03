69ab21e4e62f73b4a4dd879b868fe9ecdb5a7258
Nero <286830682@qq.com>
2022-01-03 13:21:08 +0800

    Optimize memory_pool.cpp

--- templates/memory_pool.cpp
+++ templates/memory_pool.cpp
@@ -3,24 +3,36 @@ class MemoryPool {
  public:
   template<typename... Args>
   T* allocate(Args... args) {
-    if (__builtin_expect(pool_.empty(), false)) {
-      T* arr = new T[kReAllocSize];
-      for (int i = 0; i < kReAllocSize; ++i) {
-        pool_.emplace_back(arr + i);
+    if (__builtin_expect(head == nullptr, false)) {
+      char* arr = new char[kReAllocBytes];
+      for (int i = 0; i < kBatchSize; ++i) {
+        BufferList* p = reinterpret_cast<BufferList*>(arr);
+        p->next = head;
+        head = p;
+        arr += kUnitBytes;
       }
     }
 
-    T* ret = pool_.back(); pool_.pop_back();
+    T* ret = reinterpret_cast<T*>(head);
+    head = head->next;
     return new(ret) T(args...);
   }
 
   void free(T* ptr) {
-    pool_.emplace_back(ptr);
+    BufferList* p = reinterpret_cast<BufferList*>(ptr);
+    p->next = head;
+    head = p;
   }
 
  private:
-  static constexpr int kReAllocSize = 1024;
+  struct BufferList {
+    BufferList* next = nullptr;
+  };
 
-  std::vector<T*> pool_;
+  static constexpr int kUnitBytes = std::max(sizeof(T), sizeof(BufferList));
+  static constexpr int kBatchSize = 1024;
+  static constexpr int kReAllocBytes = kUnitBytes * kBatchSize;
+
+  BufferList* head = nullptr;
 };
 
