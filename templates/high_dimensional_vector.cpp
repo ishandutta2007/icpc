494a5ca13e2e690cef1815ced3b2683148ffcdcb
ssstare <286830682@qq.com>
2022-01-05 15:14:49 +0800

    Rename `make` to `create`

--- templates/high_dimensional_vector.cpp
+++ templates/high_dimensional_vector.cpp
@@ -3,8 +3,8 @@ struct HighDimensionalVector {
   using type = std::vector<typename HighDimensionalVector<T, num_dimensions - 1>::type>;
 
   template<typename... Args>
-  static type make(int dim, Args&&... args) {
-    return type(dim, HighDimensionalVector<T, num_dimensions - 1>::make(std::forward<Args>(args)...));
+  static type create(int dim, Args&&... args) {
+    return type(dim, HighDimensionalVector<T, num_dimensions - 1>::create(std::forward<Args>(args)...));
   }
 };
 
@@ -12,8 +12,8 @@ template<typename T>
 struct HighDimensionalVector<T, 0> {
   using type = T;
 
-  static type make() { return T{}; }
-  static type make(T value) { return std::move(value); }
+  static type create() { return T{}; }
+  static type create(T value) { return std::move(value); }
 };
 
 template<typename T, int num_dimensions>
