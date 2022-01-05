17fb7d5f20ca273f272822ea20e2dbf58ea0ec19
ssstare <286830682@qq.com>
2022-01-05 14:03:14 +0800

    Update

--- templates/high_dimensional_vector.cpp
+++ templates/high_dimensional_vector.cpp
@@ -3,8 +3,8 @@ struct HighDimensionalVector {
   using type = std::vector<typename HighDimensionalVector<T, num_dimensions - 1>::type>;
 
   template<typename... Args>
-  static type make(int dim, Args... args) {
-    return type(dim, HighDimensionalVector<T, num_dimensions - 1>::make(args...));
+  static type make(int dim, Args&&... args) {
+    return type(dim, HighDimensionalVector<T, num_dimensions - 1>::make(std::forward<Args>(args)...));
   }
 };
 
@@ -13,7 +13,7 @@ struct HighDimensionalVector<T, 0> {
   using type = T;
 
   static type make() { return T{}; }
-  static type make(const T& value) { return value; }
+  static type make(T value) { return std::move(value); }
 };
 
 template<typename T, int num_dimensions>
