df332ab823d7d5c23b02a1135a48e2d90ea0fd3b
Nero <286830682@qq.com>
2022-01-27 15:44:09 +0800

    update SAM

--- templates/suffix_automaton.cpp
+++ templates/suffix_automaton.cpp
@@ -2,10 +2,12 @@ struct SuffixAutomaton {
   static constexpr int kCharsetSize = 26;
   static int hash(char c) { return c - 'a'; }
 
-  explicit SuffixAutomaton(int length = 0) : pool(length * 2 + 1), alloc(pool.data()) {
-    root = last = new(alloc++) Node(0);
+  SuffixAutomaton() {
+    root = allocate(0);
+    last = allocate(1);
+    last->fa = root;
   }
-  explicit SuffixAutomaton(const std::string& str) : SuffixAutomaton(str.length()) {
+  explicit SuffixAutomaton(const std::string& str) : SuffixAutomaton() {
     for (int i = 0; i < str.length(); ++i) extend(hash(str[i]));
   }
 
@@ -13,18 +15,25 @@ struct SuffixAutomaton {
     Node* fa = nullptr;
     std::array<Node*, kCharsetSize> go = {nullptr};
     int val = 0;
+    int id = -1;
 
     explicit Node(int val = 0) : val(val) {}
   };
 
-  std::vector<Node> pool;
-  Node* alloc = nullptr;
+  template<typename... Args>
+  static Node* allocate(Args&&... args) {
+    Node* u = pool.allocate(std::forward<Args>(args)...);
+    return u;
+  }
+
+  static MemoryPool<Node> pool;
+
   Node* root = nullptr;
   Node* last = nullptr;
 
   void extend(int c) {
     Node* p = last;
-    Node* np = new(alloc++) Node(p->val + 1);
+    Node* np = allocate(p->val + 1);
     while (p && p->go[c] == 0) {
       p->go[c] = np; p = p->fa;
     }
@@ -35,7 +44,7 @@ struct SuffixAutomaton {
       if (p->val + 1 == q->val) {
         np->fa = q;
       } else {
-        Node* nq = new(alloc++) Node(p->val + 1);
+        Node* nq = allocate(p->val + 1);
         std::copy(q->go.begin(), q->go.end(), nq->go.begin());
         nq->fa = q->fa;
         q->fa = nq;
@@ -58,4 +67,6 @@ struct SuffixAutomaton {
     return true;
   }
 };
+
+MemoryPool<SuffixAutomaton::Node> SuffixAutomaton::pool;
 
