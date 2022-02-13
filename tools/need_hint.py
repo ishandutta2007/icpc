ada95ac46e8cb8dd28ab242c552e6e10cbf2dbde
Nero <286830682@qq.com>
2022-02-13 21:11:19 +0800

    Add option debug

--- tools/need_hint.py
+++ tools/need_hint.py
@@ -10,9 +10,15 @@ def main():
 
 @main.command()
 def idea():
-    with open('./data/ideas.txt', 'r') as f:
+    with open('./data/hints/ideas.txt', 'r') as f:
         ideas = f.readlines()
     print(ideas[random.randint(0, len(ideas) - 1)])
+
+@main.command()
+def debug():
+    with open('./data/hints/bugs.txt', 'r') as f:
+        bugs = f.readlines()
+    print(bugs[random.randint(0, len(bugs) - 1)])
 
 
 if __name__ == '__main__':
