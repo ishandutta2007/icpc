8b645aa61e8a739f9d7d0bbd51a37f488bdf2a4b
Nero <286830682@qq.com>
2022-03-23 20:47:04 +0800

    Format two_sat.cpp

--- snippets/two_sat.cpp
+++ snippets/two_sat.cpp
@@ -1,47 +1,54 @@
 struct TwoSat {
  public:
-  int n;
-  std::vector<int> head;
-  std::vector<int> stack;
-  std::vector<bool> mark;
+  explicit TwoSat(int n = 0) : num_variables(n), mark(num_variables << 1, false), graph(num_variables << 1) {}
 
-  struct Edge {
-    int v, next;
-  };
-  std::vector<Edge> g;
-
-  explicit TwoSat(int n) : n(n), head(n, -1), mark(n, false) {}
-
-  void add_edge(int u, int v) {
-    g.emplace_back(Edge{.v = v, .next = head[u]});
-    head[u] = (int)g.size() - 1;
+  // clause_1 \cap clause_2 \cap ... \cap clause_m,
+  // where clause is in the form of `(x_fx \cap y_fy)`,
+  // x and y are both boolean variables, and x_fx means x takes the boolean value fx.
+  void add_clause(int x, bool fx, int y, bool fy) {
+    add_edge(x << 1 | !fx, y << 1 | fy);
+    add_edge(y << 1 | !fy, x << 1 | fx);
   }
 
   bool solve() {
-    for (int i = 0; i < n; i += 2) {
-      if (!mark[i] && !mark[i + 1]) {
-        stack.clear();
-        if (!dfs(i)) {
-          while (!stack.empty()) {
-            int v = stack.back(); stack.pop_back();
-            mark[v] = false;
-          }
-          if (!dfs(i + 1)) return false;
+    for (int i = 0; i < num_variables; ++i) if (!mark[i << 1] && !mark[i << 1 | 1]) {
+      stack.clear();
+      if (!dfs(i << 1)) {
+        while (!stack.empty()) {
+          int v = stack.back(); stack.pop_back();
+          mark[v] = false;
         }
+        if (!dfs(i << 1 | 1)) return false;
       }
     }
     return true;
   }
 
+  std::vector<bool> get_solution() const {
+    std::vector<bool> solution(num_variables);
+    for (int i = 0; i < num_variables; ++i) solution[i] = mark[i << 1 | 1];
+    return solution;
+  }
+
  private:
+  int num_variables = 0;
+  std::vector<int> stack;
+  std::vector<bool> mark;
+
+  struct Edge {
+    int v;
+  };
+  std::vector<std::vector<Edge>> graph;
+
+  void add_edge(int u, int v) { graph[u].emplace_back(Edge{.v = v}); }
+
   bool dfs(int u) {
     if (mark[u ^ 1]) return false;
     if (mark[u]) return true;
     mark[u] = true;
     stack.emplace_back(u);
-    for (int i = head[u]; i != -1; i = g[i].next) {
-      int v = g[i].v;
-      if (!dfs(v)) return false;
+    for (const Edge& e : graph[u]) {
+      if (!dfs(e.v)) return false;
     }
     return true;
   }
