8a348d4f541989f84e6b0d4782cfc3ea25078179
Nero <286830682@qq.com>
2022-05-20 23:08:08 +0800

    Add gray_code.cpp

---
+++ snippets/gray_code.cpp
@@ -0,0 +1,46 @@
+struct GrayCode {
+
+  // Generate a cyclic permutation of {0, 1, .. , 2^n - 1},
+  // so that any two neighbor elements diff in exactly one bit.
+  static std::vector<int> generate(int n) {
+    assert(n > 0);
+    std::vector<int> cur{0, 1};
+    for (int i = 1; i < n; ++i) {
+      std::vector<int> nex = cur;
+      for (int j = (int)cur.size() - 1; j >= 0; --j) {
+        nex.emplace_back(cur[j] | 1 << i);
+      }
+      cur.swap(nex);
+    }
+    return cur;
+  }
+
+  template<typename T = int>
+  static T next(T mask, int n) {
+    assert(n > 0);
+    if (mask == T(1) << (n - 1)) return 0;
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) cnt += mask >> i & 1;
+    if (~cnt & 1) return mask ^ 1;
+    for (int i = 0; i < n; ++i) if (mask >> i & 1) {
+      mask ^= T(1) << (i + 1);
+      break;
+    }
+    return mask;
+  }
+
+  template<typename T = int>
+  static T prev(T mask, int n) {
+    assert(n > 0);
+    if (mask == 0) return T(1) << (n - 1);
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) cnt += mask >> i & 1;
+    if (cnt & 1) return mask ^ 1;
+    for (int i = 0; i < n; ++i) if (mask >> i & 1) {
+      mask ^= T(1) << (i + 1);
+      break;
+    }
+    return mask;
+  }
+};
+
