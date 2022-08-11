49761293a60019172d3f786a383383e4ae902fc3
Nero <286830682@qq.com>
2022-08-11 20:48:55 +0800

    Update

--- snippets/dsu.cpp
+++ snippets/dsu.cpp
@@ -7,20 +7,13 @@ struct Dsu {
   explicit Dsu(int n) : n(n), dsu(n) {
     std::iota(dsu.begin(), dsu.end(), 0);
   }
-
   int size() const { return n; }
+  int find(int x) { return dsu[x] == x ? x : dsu[x] = find(dsu[x]); }
+  bool same(int x, int y) { x = find(x); y = find(y); return x == y; }
 
-  int find(int x) {
-    return dsu[x] == x ? x : dsu[x] = find(dsu[x]);
-  }
+  bool merge(int x, int y) { return merge_from(x, y); }
 
-  bool same(int x, int y) {
-    x = find(x);
-    y = find(y);
-    return x == y;
-  }
-
-  bool merge(int x, int y) {
+  bool merge_from(int x, int y) {
     x = find(x);
     y = find(y);
     if (x == y) return false;
